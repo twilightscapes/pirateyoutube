444c73397013c85374760d6eb30e5d9472b082f1
Twilight <ihatespam@gmail.com>
2023-12-06 09:36:08 -0600

    initial

--- update-theme.sh
+++ update-theme.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Set the URL of your central repository
-THEME_REPO_URL="https://github.com/piratesocial/pirateplus"
+# THEME_REPO_URL="https://github.com/piratesocial/pirateplus"
 
-# Set the branch or tag you want to pull updates from
-BRANCH_OR_TAG="main"
+# # Set the branch or tag you want to pull updates from
+# BRANCH_OR_TAG="main"
 
-# Temporarily store the user's changes (optional)
-mv src user_src_backup
+# # Temporarily store the user's changes (optional)
+# mv src user_src_backup
 
-# Clone the central repository
-git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
+# # Clone the central repository
+# git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
 
-# Replace the src folder
-rm -rf src
-mv tmp_theme/src .
+# # Replace the src folder
+# rm -rf src
+# mv tmp_theme/src .
 
-# Copy the package.json file
-cp tmp_theme/package.json .
+# # Copy the package.json file
+# cp tmp_theme/package.json .
 
-# Clean up
-rm -rf tmp_theme
-rm -rf user_src_backup
+# # Clean up
+# rm -rf tmp_theme
+# rm -rf user_src_backup
 
 echo "Theme updated successfully!"
