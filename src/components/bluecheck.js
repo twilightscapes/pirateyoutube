63192ebf307c9d8111378495fa756e4a42a4092d
Twilight <ihatespam@gmail.com>
2023-12-07 09:21:41 -0600

    bluechecks

--- src/components/bluecheck.js
+++ src/components/bluecheck.js
@@ -1,6 +1,7 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { BsFillPatchCheckFill } from "react-icons/bs"
+import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { useLocation } from '@reach/router';
 
 const BlueCheck = () => {
   const data = useStaticQuery(graphql`
@@ -11,17 +12,20 @@ const BlueCheck = () => {
         }
       }
     }
-  `)
+  `);
 
-  const hasNetlifyApp = data.site.siteMetadata.siteUrl.includes("netlify.app")
+  const hasNetlifyApp = data.site.siteMetadata.siteUrl.includes('netlify.app');
+
+  const location = useLocation();
+  console.log(location);
 
   return (
     <span title="This site is verified">
-      {!hasNetlifyApp ? (
-        <BsFillPatchCheckFill style={{ color: "#1D9BF0" }} />
-      ) : null}
+      {hasNetlifyApp === true && (
+        <BsFillCheckCircleFill style={{ color: '#1D9BF0' }} />
+      )}
     </span>
-  )
-}
+  );
+};
 
-export default BlueCheck
+export default BlueCheck;
