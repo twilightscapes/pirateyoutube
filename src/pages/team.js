c640cbb5519e8fff818adc758947b4d2bec4aded
Twilight <ihatespam@gmail.com>
2023-12-07 10:35:20 -0600

    team

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,96 +1,57 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
-import Layout from "../components/siteLayout"
-import useSiteMetadata from "../hooks/SiteMetadata"
-import { Helmet } from "react-helmet"
-import Seo from "../components/seo"
-// import ScrollAnimation from 'react-animate-on-scroll'
-// import { ImPlay, ImDisplay, ImMobile2 } from "react-icons/im"
-// import { BiGridHorizontal } from "react-icons/bi"
-// import { MdOutlineRectangle } from "react-icons/md"
-
+import React from "react";
+import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import Layout from "../components/siteLayout";
+import useSiteMetadata from "../hooks/SiteMetadata";
+import { Helmet } from "react-helmet";
+import Seo from "../components/seo";
 
 const TeamPage = ({ data }) => {
-  const { showNav } = useSiteMetadata()
-  const posts = data.allMarkdownRemark.edges
-
-
-
-  
-  // // call the appropriate function on page load and resize
-  // window.addEventListener('load', () => {
-  //   if (window.innerWidth >= 768) {
-  //     resizeSwipe();
-  //   } else {
-  //     resizeGrid();
-  //   }
-  // });
-  
-  // window.addEventListener('resize', () => {
-  //   if (window.innerWidth >= 768) {
-  //     resizeSwipe();
-  //   } else {
-  //     resizeGrid();
-  //   }
-  // });
-  
-
+  const { showNav } = useSiteMetadata();
+  const posts = data.allMarkdownRemark.edges;
 
   return (
     <Layout className="contact-page">
       <Helmet>
-  <body className="contactpage utilitypage" />
-</Helmet>
-      <Seo
-        title=""
-        description=""
-      />
+        <body className="contactpage utilitypage" />
+      </Helmet>
+      <Seo title="" description="" />
 
-
-
-
-
-
-{showNav ? (
-    <div className="spacer" style={{height:'60px', border:'0px solid yellow'}}></div>
-        ) : (
-          ""
-        )}
-      <div>
-
-
-
-        <div className="contentpanel grid-container" style={{padding:''}}>
-          
-        <div className="sliderSpacer" style={{height:'', paddingTop:'', display:''}}></div>
-        
-        {posts.map(({ node }) => (
-  <div className="post-card1 grid-item"
-  style={{  alignItems:'center'}} key={node.id}>
-    <a href={node.frontmatter.slug}>
-      {node.frontmatter.profilePicture && (
-        <GatsbyImage
-          image={node.frontmatter.profilePicture.childImageSharp.gatsbyImageData}
-          alt={node.frontmatter.name}
-        />
+      {showNav ? (
+        <div className="spacer" style={{ height: '60px', border: '0px solid yellow' }}></div>
+      ) : (
+        ''
       )}
-      <h2>{node.frontmatter.name}</h2>
-    </a>
-  </div>
-))}
+      <div>
+        <div className="contentpanel grid-container" style={{ padding: '' }}>
+          <div className="sliderSpacer" style={{ height: '', paddingTop: '', display: '' }}></div>
 
+          {posts.map(({ node }) => (
+            <div className="post-card1 grid-item" style={{ alignItems: 'center' }} key={node.id}>
+              <a href={node.frontmatter.slug}>
+                {node.frontmatter.profilePicture && (
+                  <GatsbyImage
+                    image={node.frontmatter.profilePicture.childImageSharp.gatsbyImageData}
+                    alt={node.frontmatter.name}
+                  />
+                )}
+                <h2>{node.frontmatter.name}</h2>
+                <p>{node.frontmatter.jobTitle}</p>
+                {/* Add additional details as needed */}
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query {
     allMarkdownRemark(
-      filter: {fileAbsolutePath: {regex: "/team/"}}
-      sort: {frontmatter: {order: ASC}}
+      filter: { fileAbsolutePath: { regex: "/team/" } }
+      sort: { frontmatter: { order: ASC } }
     ) {
       edges {
         node {
@@ -115,7 +76,4 @@ export const query = graphql`
   }
 `;
 
-
-
-
-export default TeamPage
+export default TeamPage;
---
+++ static/content/team/tom.md
@@ -0,0 +1,12 @@
+---
+slug: /team/tom
+name: Tom
+jobTitle: Senior Tom, Associate
+credentials: ZZ
+order: 1
+profilePicture: /assets/default-user.webp
+template: team
+showDetailedPage: true
+list: true
+---
+Some text goes here
