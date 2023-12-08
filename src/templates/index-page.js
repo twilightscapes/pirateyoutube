9d6ab934791c72f0de2d47cbbc1ba1ca83bdc7f7
Twilight <ihatespam@gmail.com>
2023-12-08 01:02:36 -0600

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,51 +1,46 @@
+// src/templates/index-page.js
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/siteLayout';
 import HomePosts from '../components/homeposts';
 import { Helmet } from 'react-helmet';
 import useSiteMetadata from '../hooks/SiteMetadata';
+
 export const query = graphql`
-query HomePostsQuery($homecount: Int) {
-  allMarkdownRemark(
-    filter: { frontmatter: { template: { eq: "blog-post" } } }
-    sort: [{ frontmatter: { spotlight: DESC } }, { frontmatter: { date: DESC } }]
-    limit: $homecount
-  ) {
-    edges {
-      node {
-        fields {
-          slug
-        }
-        frontmatter {
-          date
-          title
-          tags
-          youtube {
-            youtuber
+  query HomePostsQuery($homecount: Int) {
+    allMarkdownRemark(
+      filter: { frontmatter: { template: { eq: "blog-post" } } }
+      sort: { fields: [frontmatter___spotlight, frontmatter___date], order: [DESC, DESC] }
+      limit: $homecount
+    ) {
+      edges {
+        node {
+          fields {
+            slug
           }
-          featuredImage {
-            childImageSharp {
-              gatsbyImageData(
-                quality: 80
-                placeholder: BLURRED
-                formats: [AUTO, WEBP, AVIF]
-              )
+          frontmatter {
+            date
+            title
+            tags
+            youtube {
+              youtuber
+            }
+            featuredImage {
+              childImageSharp {
+                gatsbyImageData(quality: 80, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+              }
             }
           }
         }
       }
     }
-    group(field: { frontmatter: { tags: SELECT } }) {
-      fieldValue
-    }
   }
-}
-
 `;
 
-
 const HomePage = ({ data, pageContext }) => {
-  const { showNav, siteUrl } = useSiteMetadata();
+  const { showNav } = useSiteMetadata();
+  const { siteUrl } = useSiteMetadata();
+  const { homecount } = pageContext;
 
   return (
     <Layout>
@@ -54,9 +49,9 @@ const HomePage = ({ data, pageContext }) => {
       </Helmet>
       {siteUrl}
       {showNav ? (
-        <div className="spacer" style={{ height: '70px', border: '0px solid yellow' }}></div>
+        <div className='spacer' style={{ height: '70px', border: '0px solid yellow' }}></div>
       ) : (
-        <div className="spacer2" style={{ height: '0', border: '0px solid yellow' }}></div>
+        <div className="spacer2" style={{ height: "0", border: "0px solid yellow" }}></div>
       )}
 
       <HomePosts data={data} />
