251603c9556aa8e56673a3b137532b7a5fd9810e
Twilight <ihatespam@gmail.com>
2023-12-08 04:11:35 -0600

    Update update-theme.sh

--- update-theme.sh
+++ update-theme.sh
@@ -1,19 +1,13 @@
 # !/bin/bash
 
-# Check for uncommitted changes
-if ! git diff-index --quiet HEAD --; then
-  echo "Error: You have uncommitted changes. Please commit or stash them before updating."
-  exit 1
-fi
-
-# Backup user changes
-mv src user_src_backup
-
 # Set the URL of your central repository
 THEME_REPO_URL="https://github.com/piratesocial/pirateplus"
 
 # Set the branch or tag you want to pull updates from
 BRANCH_OR_TAG="main"
+
+# Backup user changes
+mv src user_src_backup
 
 # Clone the central repository
 git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
@@ -21,6 +15,15 @@ git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
 # Replace the src folder
 rm -rf src
 mv tmp_theme/src .
+
+# Replace the gatsby-config.js file
+cp tmp_theme/gatsby-config.js .
+
+# Replace the gatsby-node.js file
+cp tmp_theme/gatsby-node.js .
+
+# Replace the netlify.toml file
+cp tmp_theme/netlify.toml .
 
 # Update the admin/config.yml file
 cp tmp_theme/static/admin/config.yml static/admin/
@@ -34,4 +37,5 @@ rm -rf tmp_theme
 echo "Theme updated successfully!"
 
 
-echo "No Theme Updated - Needed"
+
+# echo "No Theme Updated - Needed"
