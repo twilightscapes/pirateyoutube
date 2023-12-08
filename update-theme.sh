9e94f84df4841afe0cbe7f441c89eb56598a26f8
Twilight <ihatespam@gmail.com>
2023-12-08 03:58:13 -0600

    down 2 5.10.0

--- update-theme.sh
+++ update-theme.sh
@@ -1,37 +1,37 @@
-#!/bin/bash
+# !/bin/bash
 
-# # Check for uncommitted changes
-# if ! git diff-index --quiet HEAD --; then
-#   echo "Error: You have uncommitted changes. Please commit or stash them before updating."
-#   exit 1
-# fi
+# Check for uncommitted changes
+if ! git diff-index --quiet HEAD --; then
+  echo "Error: You have uncommitted changes. Please commit or stash them before updating."
+  exit 1
+fi
 
-# # Backup user changes
-# mv src user_src_backup
+# Backup user changes
+mv src user_src_backup
 
-# # Set the URL of your central repository
-# THEME_REPO_URL="https://github.com/piratesocial/pirateplus"
+# Set the URL of your central repository
+THEME_REPO_URL="https://github.com/piratesocial/pirateplus"
 
-# # Set the branch or tag you want to pull updates from
-# BRANCH_OR_TAG="main"
+# Set the branch or tag you want to pull updates from
+BRANCH_OR_TAG="main"
 
-# # Clone the central repository
-# git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
+# Clone the central repository
+git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
 
-# # Replace the src folder
-# rm -rf src
-# mv tmp_theme/src .
+# Replace the src folder
+rm -rf src
+mv tmp_theme/src .
 
-# # Update the admin/config.yml file
-# cp tmp_theme/static/admin/config.yml static/admin/
+# Update the admin/config.yml file
+cp tmp_theme/static/admin/config.yml static/admin/
 
-# # Copy the package.json file
-# cp tmp_theme/package.json .
+# Copy the package.json file
+cp tmp_theme/package.json .
 
-# # Clean up
-# rm -rf tmp_theme
+# Clean up
+rm -rf tmp_theme
 
-# echo "Theme updated successfully!"
+echo "Theme updated successfully!"
 
 
 echo "No Theme Updated - Needed"
