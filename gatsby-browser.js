ebb70ef081fe73039d958fc2a8b64f9892eea9c2
Twilight <ihatespam@gmail.com>
2023-12-12 05:52:13 -0600

    break fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,4 @@
-export const onInitialClientRender = () => {
-    setTimeout(function() {
-        document.getElementById("___loader").style.display = "none"
-    }, 1000)
-  }
-  
+
   
   
   export const onServiceWorkerUpdateReady = () => {
