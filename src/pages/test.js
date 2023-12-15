2ac061cbc0579e5f6a0b0a984af200d84d90070b
Twilight <ihatespam@gmail.com>
2023-12-15 10:12:41 -0600

    Revert "new swipe test"
    This reverts commit b0d7c8a6f3811ea7b0835d7a136ac0e59366c837.

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react"
+import React from "react"
 // import useSiteMetadata from "../hooks/SiteMetadata"
 import Seo from "../components/seo"
 import Layout from "../components/siteLayout"
@@ -7,72 +7,101 @@ import Layout from "../components/siteLayout"
 import styled from "styled-components"
 const CustomBox = styled.div`
 
-.horizontal-scroll1 {
-  display: flex;
-  overflow-x: scroll;
-  -webkit-overflow-scrolling: touch; /* Enable smooth scrolling on iOS */
-max-width: 80vw;  margin: 5vh auto;
-  max-height: 80vh;
-}
-
-.slider {
-  display: flex;
-  scroll-snap-type: x mandatory;
-  flex-shrink: 0;
-  width: 100vw; /* Set the width of each post-card1 to the viewport width */
-}
-
-.post-card1 {
-  flex: 0 0 100%;
-  min-height: 80vh;
-  border: 1px solid #ddd;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
-  display: grid;
-  place-content: center;
-}
 `
 
-const HorizontalScroll = () => {
-  useEffect(() => {
-    const handleWheel = (event) => {
-      event.preventDefault();
-      document.querySelector(".horizontal-scroll1").scrollLeft += event.deltaY;
-    };
-
-    const sliderContainer = document.querySelector(".horizontal-scroll1");
-
-    if (sliderContainer) {
-      sliderContainer.addEventListener("wheel", handleWheel);
-    }
-
-    return () => {
-      
-      // Remove the event listener when the component is unmounted
-      if (sliderContainer) {
-        sliderContainer.removeEventListener("wheel", handleWheel);
-      }
-    };
-  }, []); // Empty dependency array ensures the effect runs once after mount
+function Test () {
 
   return (
-  <CustomBox>
-    <Layout>
-      <Seo title="Test Page" />
-    <div className="horizontal-scroll1">
-      <div className="slider">
-        <div className="post-card1" style={{ backgroundColor: '#ff0000', color: '#ddd' }}>post-card1 1</div>
-        <div className="post-card1" style={{ backgroundColor: '#2b61a8', color: '#ddd' }}>post-card1 2</div>
-        <div className="post-card1" style={{ backgroundColor: '#156e5e', color: '#ddd' }}>post-card1 3</div>
+
+  <Layout className="not-found-page">
+    <Seo title="Page not found" />
+
+      <header className=""></header>
+
+        {/* <AiFillRobot
+          style={{
+            fontSize: "160px",
+            color: "var(--primary-color)",
+            margin:'0 auto',
+        textAlign:'center'
+          }}
+        /> */}
+
+
+
+{/* <div className="scroll-container1" style={{display:'flex', justifyContent:'start', maxWidth:'', height:'calc(100vh - 70px)', margin:'0 auto 0 auto', position:'relative', left:'0', right:'0', top:'0'}}> */}
+
+<div className="horizontal-scroll-container">
+      <div className="horizontal-scroll-content">
+        {/* Your content goes here */}
+        <div className="section">Section 1</div>
+        <div className="section">Section 2</div>
+        <div className="section">Section 3</div>
+        {/* Add more sections as needed */}
       </div>
     </div>
-    </Layout>
-    </CustomBox>
+
+
+
+
+{/* </div> */}
+
+  </Layout>
   );
-};
+      }
 
-export default HorizontalScroll;
-
-
+export default Test
 
 
+
+// import React from "react"
+// import { Link } from "gatsby"
+// import { RiArrowLeftSLine, RiBugLine } from "react-icons/ri"
+// import { StaticImage } from "gatsby-plugin-image"
+// // import { AiFillRobot } from "react-icons/ai"
+// import Seo from "../components/seo"
+// import Layout from "../components/siteLayout"
+
+// const NotFound = () => (
+//   <Layout className="not-found-page">
+//     <Seo title="Page not found" />
+
+//       <header className="tvscreen"></header>
+
+//         {/* <AiFillRobot
+//           style={{
+//             fontSize: "160px",
+//             color: "var(--primary-color)",
+//             margin:'0 auto',
+//         textAlign:'center'
+//           }}
+//         /> */}
+//  <div style={{display:'grid', justifyContent:'center', gap:'30px', height:'100vh', width:'100vw',  border:'0px solid', background:'rgba(0, 0, 0, .8)'}}>
+
+  
+
+//  <StaticImage src="../img/moose.webp" alt="Todd builds Web Apps" style={{height:'auto', position:'fixed', top:'80px', left:'0', zIndex:'', width:'100vw', maxHeight:'60%',  objectFit:'contain', overflow:'',}}  />
+
+
+// {/* <h1 className="headline1" style={{fontSize:'200%'}}>Well, Darn.</h1> */}
+// <p className="headline1" style={{fontSize:'', margin:'2rem auto'}}>
+  
+// </p>
+
+// <div style={{display:'flex',}}>
+//         <Link to="/" className="button">
+//         <RiArrowLeftSLine className="icon -left" />
+//         Back to Homepage
+//       </Link>
+//       <Link to="/contact" className="button">
+//         Report this &nbsp;<RiBugLine className="icon -right" />
+//       </Link></div>
+
+      
+//       </div>
+
+
+//   </Layout>
+// )
+
+// export default NotFound
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -11,37 +11,6 @@ import TimeAgo from 'react-timeago';
 import { MdArrowForwardIos } from 'react-icons/md';
 import Seo from "../components/seo";
 import { getSrc } from "gatsby-plugin-image";
-import styled from "styled-components"
-const CustomBox = styled.div`
-
-.horizontal-scroll1 {
-  display: flex;
-  overflow-x: scroll;
-  -webkit-overflow-scrolling: touch; /* Enable smooth scrolling on iOS */
-// max-width: 80vw;  
-// margin: 5vh auto;
-  // max-height: 80vh;
-  // justifyContent: start;
-}
-
-.slider {
-  display: flex;
-  scroll-snap-type: x mandatory;
-  flex-shrink: 0;
-  width: 100vw; /* Set the width of each post-card1 to the viewport width */
-}
-
-.post-card12 {
-  flex: 0 0 100%;
-  min-height: 100vh;
-  // border: 1px solid #ddd;
-  // box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
-  display: grid;
-  place-content: center;
-}
-`
-
 
 const HomePage = ({ data }) => {
   const { showModals, showDates, homecount, postcount, magicOptions, showNav, showArchive  } = useSiteMetadata();
@@ -111,30 +80,7 @@ const HomePage = ({ data }) => {
     setVisibleItems(homecount);
   }
 
-  useEffect(() => {
-    const handleWheel = (event) => {
-      event.preventDefault();
-      document.querySelector(".horizontal-scroll1").scrollLeft += event.deltaY;
-    };
-
-    const sliderContainer = document.querySelector(".horizontal-scroll1");
-
-    if (sliderContainer) {
-      sliderContainer.addEventListener("wheel", handleWheel);
-    }
-
-    return () => {
-      
-      // Remove the event listener when the component is unmounted
-      if (sliderContainer) {
-        sliderContainer.removeEventListener("wheel", handleWheel);
-      }
-    };
-  }, []); // Empty dependency array ensures the effect runs once after mount
-
-  
   return (
-    <CustomBox>
     <Layout>
       <Helmet>
         <body id="body" className="homepage" />
@@ -274,14 +220,14 @@ const HomePage = ({ data }) => {
       )}
 
 
-<div className="contentpanel1 horizontal-scroll1" style={{ justifyContent: 'center', alignItems: 'center', marginTop: showNav ? '' : '7vh' }}>
+<div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', marginTop: showNav ? '' : '7vh' }}>
 
 
-<div className="slider">
-        {/* <div className="sliderSpacer" style={{ height: '', paddingTop: '', display: '' }}></div> */}
+
+        <div className="sliderSpacer" style={{ height: '', paddingTop: '', display: '' }}></div>
 
         {filteredPosts.slice(0, numVisibleItems).map(({ node }, index) => (
-          <div key={index} className="post-card12" style={{ alignItems: 'center' }}>
+          <div key={index} className="post-card1" style={{ alignItems: 'center' }}>
             <Link className="postlink" state={showModals ? { modal: true } : {}} key={node.frontmatter.slug} to={node.frontmatter.slug}>
               <div>
                 {node.frontmatter.featuredImage ? (
@@ -343,14 +289,13 @@ const HomePage = ({ data }) => {
   </div>
 )}
 
-</div>
+
 
 
 
 
       </div>
     </Layout>
-    </CustomBox>
   );
 };
 
