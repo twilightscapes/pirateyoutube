a8f57f0e0e5863c0350f66734d232fe57ac67567
Twilight <ihatespam@gmail.com>
2023-12-17 01:17:00 -0600

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -222,7 +222,7 @@ const HomePage = ({ data }) => {
 
 {/* <div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', marginTop: true ? '7vh' : '0' }}> */}
 
-<div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', paddingTop: showNav ? '4vw' : '4vw', }}>
+<div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', paddingTop: showNav ? '6vw' : '6vw', }}>
 
 
 
