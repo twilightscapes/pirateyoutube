cd2ec968c2de096108828def07c28694cc43fb86
Twilight <ihatespam@gmail.com>
2023-12-22 02:09:14 -0600

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -355,9 +355,9 @@ const HomePage = ({ data }) => {
 ) : (       
 <> */}
 
-<div className="panel" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', alignContent:'center', margin: '10px auto', maxWidth: '80vw', gap: '.4vw', maxHeight: '', textAlign: 'center', padding: '1vh 1vw', fontSize: 'clamp(.7rem,.8vw,.9rem)', background: 'rgba(0, 0, 0, 0.3)', outline:'1px solid #444', opacity:'.9' }}>
+<div className="panel" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', alignContent:'center', margin: '10px auto', maxWidth: '80vw', gap: '.4vw', maxHeight: '74px', textAlign: 'center', padding: '10px 10px', fontSize: 'clamp(.7rem,.7vw,.7rem)', background: 'rgba(0, 0, 0, 0.3)', outline:'1px solid #444', opacity:'.9', overFlow:'hidden', lineHeight:'2vh' }}>
   {showTitles ? (    
-    <h2 className="title1" style={{width:'100%'}}>{node.frontmatter.title}</h2>
+    <h2 className="title1" style={{width:'100%', height:'',}}>{node.frontmatter.title}</h2>
     ) : (
   ""
 )}
