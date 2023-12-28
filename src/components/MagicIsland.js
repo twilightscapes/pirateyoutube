6afec32de32f1acb1c0142bccf0b1425fd6c8456
Twilight <ihatespam@gmail.com>
2023-12-28 05:57:50 -0600

    sorted tags

--- src/components/MagicIsland.js
+++ src/components/MagicIsland.js
@@ -1,3 +1,4 @@
+// MagicIsland.js
 import React, { useState, useEffect } from 'react';
 
 const MagicIsland = ({ onSearch, tags, categories }) => {
@@ -21,14 +22,39 @@ const MagicIsland = ({ onSearch, tags, categories }) => {
     setSearchInput(selectedCategory ? `category:${selectedCategory}` : '');
   };
 
+  const countOccurrences = (arr) => {
+    return arr.reduce((acc, item) => {
+      acc[item] = (acc[item] || 0) + 1;
+      return acc;
+    }, {});
+  };
+
+  const sortOptions = (options, counts) => {
+    return options.sort((a, b) => counts[b.label] - counts[a.label] || a.label.localeCompare(b.label));
+  };
+
+  const sortedCategories = sortOptions(
+    categories.map((category) => ({
+      label: category,
+    })),
+    categoryCounts
+  );
+
+  const sortedTags = sortOptions(
+    tags.map((tag) => ({
+      label: tag,
+    })),
+    tagCounts
+  );
+
   return (
     <div className="cattags" style={{ maxWidth: '', margin: '0px auto 0 auto', display: 'flex', placeSelf: 'center', gap: '8px', outline: '1px solid #333', borderRadius: '3px', padding: '8px', color: '' }}>
       <div className="category-selector" style={{ marginTop: '' }}>
         <select className="" onChange={handleCategoryChange} value={searchInput.startsWith('category:') ? searchInput.replace('category:', '') : ''} style={{ background: '#222', border: '1px solid #000', borderRadius: '3px', padding: '2px' }}>
           <option value="">Category</option>
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
+          {sortedCategories.map((category) => (
+            <option key={category.label} value={category.label}>
+              {category.label}
             </option>
           ))}
         </select>
@@ -37,9 +63,9 @@ const MagicIsland = ({ onSearch, tags, categories }) => {
       <div className="tag-selector" style={{ marginTop: '' }}>
         <select className="" onChange={handleTagChange} value={searchInput.startsWith('tag:') ? searchInput.replace('tag:', '') : ''} style={{ background: '#222', border: '1px solid #000', borderRadius: '3px', padding: '2px' }}>
           <option value="">Keyword</option>
-          {tags.map((tag) => (
-            <option key={tag} value={tag}>
-              {tag}
+          {sortedTags.map((tag) => (
+            <option key={tag.label} value={tag.label}>
+              {tag.label}
             </option>
           ))}
         </select>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -11,28 +11,16 @@ import TimeAgo from 'react-timeago';
 import { MdArrowForwardIos } from 'react-icons/md';
 import Seo from "../components/seo";
 import { getSrc } from "gatsby-plugin-image";
-import ReactPlayer from 'react-player/lazy'
-// import BlogPosts from "../components/BlogPosts"; 
+import ReactPlayer from 'react-player/lazy';
 
 const HomePage = ({ data }) => {
-  const { showModals, showDates, homecount, postcount, magicOptions, showNav, showArchive, showTitles  } = useSiteMetadata();
+  const { showModals, showDates, homecount, postcount, magicOptions, showNav, showArchive, showTitles } = useSiteMetadata();
   const { showMagic, showMagicCat, showMagicTag, showMagicSearch } = magicOptions;
 
   const { markdownRemark } = data;
-  const { frontmatter, excerpt } = markdownRemark
-
-
-
-  // const showVidOnly = frontmatter.youtube.showVidOnly
-  
-  // const extractVideoId = (url) => {
-  //   const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
-  //   return match ? match[1] : null;
-  // };
+  const { frontmatter, excerpt } = markdownRemark;
 
   const allPosts = data.allMarkdownRemark.edges;
-
-
 
   const [query, setQuery] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -43,23 +31,31 @@ const HomePage = ({ data }) => {
   const allCategoriesSet = new Set(allPosts.flatMap(({ node }) => node.frontmatter.category));
   const allCategories = Array.from(allCategoriesSet);
 
-  // const AutoStart = new Set(allPosts.flatMap(({ node }) => node.frontmatter.youtube.youtubeautostart));
-
-  // // const AutoStart = node.frontmatter.youtube.youtuber}
-
-  // const ShowControls = new Set(allPosts.flatMap(({ node }) => node.frontmatter.youtube.youtubecontrols));
-
-  const allTagsSet = new Set(allPosts.flatMap(({ node }) => node.frontmatter.tags));
+  const allTagsSet = new Set(allPosts.flatMap(({ node }) => node.frontmatter.tags || []));
   const allTags = Array.from(allTagsSet);
 
   const filteredPosts = allPosts.filter(({ node }) => {
     const { title, tags, category: categories } = node.frontmatter;
     const titleMatch = query === "" || title.toLowerCase().includes(query.toLowerCase());
-    const categoryMatch = selectedCategory === "" || (Array.isArray(categories) ? categories.includes(selectedCategory) : categories === selectedCategory);
-    const tagMatch = selectedTag === "" || (tags && tags.includes(selectedTag));
+    const categoryMatch = selectedCategory === "" || (Array.isArray(categories) && categories.includes(selectedCategory));
+    const tagMatch = selectedTag === "" || (tags && Array.isArray(tags) && tags.includes(selectedTag));
+    
 
     return titleMatch && categoryMatch && tagMatch;
   });
+
+  const sortedTags = allTags
+  .filter(tag => tag)
+  .sort((a, b) => {
+    const countA = allPosts.filter(({ node }) => (node.frontmatter.tags || []).includes(a)).length;
+    const countB = allPosts.filter(({ node }) => (node.frontmatter.tags || []).includes(b)).length;
+
+    return countB - countA;
+  });
+
+
+
+
 
   useEffect(() => {
     setVisibleItems(homecount);
@@ -90,8 +86,6 @@ const HomePage = ({ data }) => {
   const showMoreItems = () => {
     setNumVisibleItems((prevNumVisibleItems) => prevNumVisibleItems + postcount);
   };
-  
-  
 
   function clearfield() {
     document.querySelector('#clearme').value = '';
@@ -100,22 +94,6 @@ const HomePage = ({ data }) => {
     setSelectedTag('');
     setVisibleItems(homecount);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return (
     <Layout>
@@ -163,36 +141,37 @@ const HomePage = ({ data }) => {
                 ""
               )}
 
-              {showMagicTag ? (
-                <>
-                  {allTags.length > 1 && (
-                    <select
-                      value={selectedTag}
-                      onChange={handleTagChange}
-                      style={{
-                        background: '#222',
-                        outline: '1px solid #111',
-                        borderRadius: '3px',
-                        padding: '2px',
-                        minWidth: '80px',
-                        maxWidth: '30%',
-                        overflow: 'hidden',
-                      }}
-                      aria-label="Select Keyword"
-                    >
-                      <option value="">Keyword</option>
-                      {allTags.filter(tag => tag).map((tag, index) => (
-                        <option key={`${tag}_${index}`} value={tag.trim()}>
-                          {tag.trim()}
-                        </option>
-                      ))}
-                    </select>
-                  )}
-                </>
-                
-              ) : (
-                ""
-              )}
+{showMagicTag ? (
+  <>
+    {sortedTags.length > 1 && (
+      <select
+        value={selectedTag}
+        onChange={handleTagChange}
+        style={{
+          background: '#222',
+          outline: '1px solid #111',
+          borderRadius: '3px',
+          padding: '2px',
+          minWidth: '80px',
+          maxWidth: '30%',
+          overflow: 'hidden',
+        }}
+        aria-label="Select Keyword"
+      >
+        <option value="">Keyword</option>
+        {sortedTags.map((tag, index) => (
+          <option key={`${tag}_${index}`} value={tag.trim()}>
+            {tag.trim()} ({allPosts.filter(({ node }) => (node.frontmatter.tags || []).includes(tag)).length})
+          </option>
+        ))}
+      </select>
+    )}
+  </>
+) : (
+  ""
+)}
+
+
 
               {showMagicSearch ? (
                 <>
@@ -257,156 +236,103 @@ const HomePage = ({ data }) => {
         ""
       )}
 
-
-
-{/* <div className="post-container">
-        <BlogPosts />
-      </div> */}
-
-
-{/* <div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', marginTop: true ? '7vh' : '0' }}> */}
-
-<div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', paddingTop: showNav ? '6vw' : '6vw', }}>
-
-
-
+      <div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', paddingTop: showNav ? '6vw' : '6vw', }}>
         <div className="sliderSpacer" style={{ height: '', paddingTop: '', display: '' }}></div>
 
         {filteredPosts.slice(0, numVisibleItems).map(({ node }, index) => (
-
           <div key={index} className="post-card1" style={{ alignItems: '', overFlow:'visible' }}>
-
             <Link className="postlink" state={showModals ? { modal: true } : {}} key={node.frontmatter.slug} to={node.frontmatter.slug}>
-
-
-
-
-
-
-
-            
-  
               {node.frontmatter.youtube.showVidOnly ? (
+                <ReactPlayer
+                  url={node.frontmatter.youtube.youtuber}
+                  allow="web-share"
+                  style={{ position: 'relative', margin: '0 auto 15px auto', zIndex: '' }}
+                  width="350px"
+                  height="200px"
+                  className='inline'
+                  playsinline
+                  config={{
+                    file: {
+                      attributes: {
+                        crossOrigin: "anonymous",
+                      },
+                    },
+                    youtube: {
+                      playerVars: { showinfo: 1, autoplay: 0, controls: 1, mute: 1, loop: 1 },
+                    },
+                  }}
+                />
+              ) : (
+                <div>
+                  {node.frontmatter.featuredImage ? (
+                    <GatsbyImage
+                      image={node.frontmatter.featuredImage.childImageSharp.gatsbyImageData}
+                      alt={node.frontmatter.title + " - Featured image"}
+                      className="featured-image1"
+                      placeholder="blurred"
+                      style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto' }}
+                    />
+                  ) : (
+                    <StaticImage
+                      className="featured-image1"
+                      src="../../static/assets/default-og-image.webp"
+                      alt="Default Image"
+                      style={{ position: 'relative', zIndex: '' }}
+                    />
+                  )}
+                </div>
+              )}
 
-<ReactPlayer
-url={node.frontmatter.youtube.youtuber}
-allow="web-share"
-style={{ position: 'relative', margin: '0 auto 15px auto', zIndex: '' }}
-width="350px"
-height="200px"
-className='inline'
-// light={`https://i.ytimg.com/vi/${extractVideoId(node.frontmatter.youtube.youtuber)}/hqdefault.jpg`}
-playsinline
-config={{
-  file: {
-    attributes: {
-      crossOrigin: "anonymous",
-    },
-  },
-  youtube: {
-    playerVars: { showinfo: 1, autoplay: 0, controls: 1, mute: 1, loop: 1 },
-  },
-}}
-/>
-          
-) : (
-  <div>
-                {node.frontmatter.featuredImage ? (
-                  <GatsbyImage
-                    image={node.frontmatter.featuredImage.childImageSharp.gatsbyImageData}
-                    alt={node.frontmatter.title + " - Featured image"}
-                    className="featured-image1"
-                    placeholder="blurred"
-                    // loading="eager"
-                    style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto' }}
-                  />
-                ) : (
-                  <StaticImage
-                    className="featured-image1"
-                    src="../../static/assets/default-og-image.webp"
-                    alt="Default Image"
-                    style={{ position: 'relative', zIndex: '' }}
-                  />
-                )}
-              </div>
-)}
-              
-
-
-  <div className="post-content" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100%', height: '', position: 'relative', background: '', padding: '', margin: '0 auto 0 auto', textAlign: 'center', overFlow: 'hidden' }}>
-
-
-              {node.frontmatter.youtube.showVidOnly ? (
-
+              <div className="post-content" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100%', height: '', position: 'relative', background: '', padding: '', margin: '0 auto 0 auto', textAlign: 'center', overFlow: 'hidden' }}>
+                {node.frontmatter.youtube.showVidOnly ? (
                   ""
-      
-      ) : (
+                ) : (
+                  <>
+                    {node.frontmatter.youtube.youtuber ? (
+                      <div className="spotlight" style={{ marginLeft: '10%', marginTop: '-28%', margin: '-24% 10% 0 10%' }}>
+                        <div className="posticons" style={{ flexDirection: 'column', margin: '0 auto' }}>
+                          <div style={{ display: 'flex', justifyContent: 'space-around', gap: '2vw', color: 'fff', }}>
+                            <FaImage className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', fontSize: '' }} />
+                            <ImPlay className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', fontSize: '' }} />
+                            <AiOutlinePicLeft className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px, fontSize: ""' }} />
+                          </div>
+                          Play Multimedia
+                        </div>
+                      </div>
+                    ) : ("")}
+                  </>
+                )}
 
-        <>
-        {node.frontmatter.youtube.youtuber ? (
-          <div className="spotlight" style={{ marginLeft: '10%', marginTop: '-28%', margin: '-24% 10% 0 10%' }}>
-            <div className="posticons" style={{ flexDirection: 'column', margin: '0 auto' }}>
-              <div style={{ display: 'flex', justifyContent: 'space-around', gap: '2vw', color: 'fff', }}>
-                <FaImage className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', fontSize: '' }} />
-                <ImPlay className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', fontSize: '' }} />
-                <AiOutlinePicLeft className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px, fontSize: ""' }} />
-              </div>
-              Play Multimedia
-            </div>
-          </div>
-        ) : ("")}
-        </>
+                <div className="panel" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', alignContent:'center', margin: '10px auto', maxWidth: '', gap: '.4vw', maxHeight: '74px', textAlign: 'left', padding: '10px 30px', fontSize: 'clamp(.7rem,.8vw,.7rem)', outline:'0px solid #444', overFlow:'hidden', lineHeight:'2.4vh', borderRadius:'3px', background: showTitles ? 'rgba(0, 0, 0, 0.8)' : 'transparent', }}>
+                  {showTitles ? (
+                    <h2 className="title1" style={{width:'100%', textShadow:'0 1px 1px #222',}}>{node.frontmatter.title}</h2>
+                  ) : (
+                    ""
+                  )}
 
-      )}
-
-
-{/* {node.frontmatter.youtube.showVidOnly ? (
-""
-) : (       
-<> */}
-
-<div className="panel" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', alignContent:'center', margin: '10px auto', maxWidth: '', gap: '.4vw', maxHeight: '74px', textAlign: 'left', padding: '10px 30px', fontSize: 'clamp(.7rem,.8vw,.7rem)', outline:'0px solid #444', overFlow:'hidden', lineHeight:'2.4vh', borderRadius:'3px', 
-background: showTitles ? 'rgba(0, 0, 0, 0.8)' : 'transparent',
-}}>
-  {showTitles ? (    
-    <h2 className="title1" style={{width:'100%', textShadow:'0 1px 1px #222',}}>{node.frontmatter.title}</h2>
-    ) : (
-  ""
-)}
-
-{showDates ? (
-    <p style={{ position: '', textAlign: 'center', border: '0px solid red', fontSize: '', padding:'0', margin:'0 0 0 20px', maxWidth: '60px', lineHeight:'100%' }}>
-      <TimeAgo date={node.frontmatter.date} />
-    </p>
-    ) : ("")}
-
-</div>
+                  {showDates ? (
+                    <p style={{ position: '', textAlign: 'center', border: '0px solid red', fontSize: '', padding:'0', margin:'0 0 0 20px', maxWidth: '60px', lineHeight:'100%' }}>
+                      <TimeAgo date={node.frontmatter.date} />
+                    </p>
+                  ) : ("")}
+                </div>
               </div>
             </Link>
           </div>
-
         ))}
 
-{numVisibleItems < filteredPosts.length && (
-  <div className="loadmore" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', placeSelf: 'center', gap: '',  textAlign: 'center' }}>
-
-    <button className="button load-more" onClick={showMoreItems} style={{maxWidth:''}}>
-      Load more
-    </button>
-
-    {showArchive ? (
-  <Link to="/archive" style={{ background: 'rgba(0, 0, 0, 0.8)', borderRadius: '5px', color: '#fff', display: 'flex', padding: '0 1vh', margin: '0 auto' }}>View Archive &nbsp;<MdArrowForwardIos style={{ marginTop: '4px' }} /></Link>
-) : (
-""
-)}
-  </div>
-)}
-
-
-
-
-
-
+        {numVisibleItems < filteredPosts.length && (
+          <div className="loadmore" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', placeSelf: 'center', gap: '',  textAlign: 'center' }}>
+            <button className="button load-more" onClick={showMoreItems} style={{maxWidth:''}}>
+              Load more
+            </button>
+            {showArchive ? (
+              <Link to="/archive" style={{ background: 'rgba(0, 0, 0, 0.8)', borderRadius: '5px', color: '#fff', display: 'flex', padding: '0 1vh', margin: '0 auto' }}>View Archive &nbsp;<MdArrowForwardIos style={{ marginTop: '4px' }} /></Link>
+            ) : (
+              ""
+            )}
+          </div>
+        )}
       </div>
     </Layout>
   );
