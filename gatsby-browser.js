0c2a00d0f889b78a0699cc10096ddec9711c46cc
Twilight <ihatespam@gmail.com>
2023-12-28 05:08:46 -0600

    tweaks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,16 +5,16 @@
 //   <ViewProvider>{element}</ViewProvider>
 // );
 
-// export const onServiceWorkerUpdateReady = () => {
-//   // Check if window is defined before using it
-//   if (typeof window !== "undefined") {
-//     const answer = window.confirm(
-//       `This website has been updated since your last visit. ` +
-//         `Reload to display the latest version?`
-//     );
+export const onServiceWorkerUpdateReady = () => {
+  // Check if window is defined before using it
+  if (typeof window !== "undefined") {
+    const answer = window.confirm(
+      `This website has been updated since your last visit. ` +
+        `Reload to display the latest version?`
+    );
 
-//     if (answer === true) {
-//       window.location.reload();
-//     }
-//   }
-// };
+    if (answer === true) {
+      window.location.reload();
+    }
+  }
+};
