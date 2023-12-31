0748ad68e2c122ad6d583d51efc54b215938e8fa
Twilight <ihatespam@gmail.com>
2023-12-31 07:46:23 -0600

    DeCap

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,5 @@
 
 import CMS from "decap-cms-app"
-
-/**
- * Any imported styles should be automatically be applied to the editor preview
- * pane thus eliminating the need to use `registerPreviewStyle` for imported
- * styles. However if you are experiencing build errors regarding importing css,
- * sass or scss into a cms module when deploying to the netlify platform, you
- * may need to follow the implementation found in netlify documentation here:
- * https://www.decapcms.org/docs/beta-features/#raw-css-in-registerpreviewstyle
- * All of the example imports below would result in styles being applied to the
- * preview pane.
- */
 // import "module-that-imports-styles.js"
 // import "styles.scss"
 // import "../other-styles.css"
@@ -27,16 +16,6 @@ import CMS from "decap-cms-app"
  */
 // CMS.registerWidget(`image-gallery`, ImageGalleryWidget, ImageGalleryPreview)
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Register 'logout' event listener
-//     netlifyIdentity.on('logout', () => {
-//       // Refresh the parent window
-//       // window.top.location.reload();
-//       window.top.location.href = '/';
-//     });
 
-//     // Register 'login' event listener
-//     netlifyIdentity.on('login', () => {
-//       window.top.location.href = '/pirate';
-//     });
-//   });
+    CMS.init({
+      })
