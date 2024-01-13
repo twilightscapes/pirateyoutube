635721ff6d5affd073e9389207df461f342d90f3
Todd <ihatespam@gmail.com>
2024-01-12 22:11:13 -0600

    archives

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import { graphql, Link, navigate } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import Layout from '../components/siteLayout';
@@ -9,11 +9,14 @@ import TimeAgo from 'react-timeago';
 // import { ImPlay } from 'react-icons/im';
 // import { FaImage } from 'react-icons/fa';
 // import { AiOutlinePicLeft } from 'react-icons/ai';
+import { ImPlay } from "react-icons/im";
+import { FaImage } from "react-icons/fa";
+import { AiOutlinePicLeft } from "react-icons/ai";
 import { MdArrowForwardIos, MdArrowBackIos } from 'react-icons/md';
 import ReactPlayer from 'react-player/lazy';
 const BlogList = ({ data, pageContext }) => {
 
-  const { postcount, featureOptions, proOptions, navOptions  } = useSiteMetadata();
+  const { postcount, featureOptions, proOptions, navOptions, language  } = useSiteMetadata();
 
 
   
@@ -21,10 +24,38 @@ const BlogList = ({ data, pageContext }) => {
   const { showDates, showTitles } = featureOptions
   const { showNav } = navOptions
   
-  // const { dicLoadMore, dicViewArchive, dicCategory, dicKeyword, dicSearch, dicClear, dicResults, dicPlayVideo, dicPlayMultimedia  } = language;
+
+  
+  const { dicPlayMultimedia, dicPlayVideo  } = language;
   
   const posts = data.allMarkdownRemark.edges;
   const { numPages, currentPage } = pageContext;
+
+
+
+
+  /* eslint-disable no-useless-escape */
+const extractVideoId = (url) => {
+  const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/[^\/\n\s]+\/(?:\S+\/)?|(?:v|e(?:mbed)?)\/|\S*?[?&]v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
+  return match ? match[1] : null;
+};
+/* eslint-enable no-useless-escape */
+  
+const playerRef = useRef(null);
+
+
+const [playingIndex, setPlayingIndex] = useState(null);
+
+  const handleVideoPlay = (index) => {
+    setPlayingIndex(index);
+  };
+
+  const handleVideoPause = () => {
+    setPlayingIndex(null);
+  };
+
+
+
 
   useEffect(() => {
     // Add any additional initialization logic if needed
@@ -37,7 +68,7 @@ const BlogList = ({ data, pageContext }) => {
   return (
     <Layout>
       <Helmet>
-        <body className="archivepage utilitypage" />
+        <body className="archivepage1 utilitypage1" />
       </Helmet>
 
       <div className="contentpanel grid-container" style={{ justifyContent: 'center', alignItems: 'center', paddingTop: showNav ? '8vw' : '8vw', }}>
@@ -45,17 +76,43 @@ const BlogList = ({ data, pageContext }) => {
 
         {posts.slice(0, currentPage * postcount).map(({ node }, index) => (
           <div className="post-card1" key={node.fields.slug} style={{ marginTop: '' }}>
-            <Link className="postlink" state={showModals ? { modal: true } : {}} key={node.frontmatter.slug} to={node.frontmatter.slug}>
+            
+            
+            {(node.frontmatter.youtube?.showVidOnly && node.frontmatter.youtube.showVidOnly) ? (
 
-{(node.frontmatter.youtube?.showVidOnly && node.frontmatter.youtube.showVidOnly) ? (
-<ReactPlayer
-                  url={node.frontmatter.youtube.youtuber}
+<div style={{minWidth:'300px', minHeight: index === playingIndex ? '200px' : '200px', background: index === playingIndex ? 'rgba(0, 0, 0, 0.5)' : 'transparent', zindex:'1'}}>
+                <ReactPlayer
+                playing={index === playingIndex}
+                ref={playerRef}
+                url={node.frontmatter.youtube.youtuber}
                   allow="web-share"
-                  style={{ position: 'relative', margin: '0 auto 15px auto', zIndex: '' }}
+                  // style={{ position: 'relative', margin: '0 auto 15px auto', zIndex: '',aspectRatio:'16/9', }}
                   width="350px"
                   height="200px"
                   className='inline'
                   playsinline
+                  // className={`relative ${index === playingIndex ? 'fixed' : 'relative'}`}
+                  style={{
+                    position: index === playingIndex ? 'fixed' : 'relative',
+                    
+                    // top: index === playingIndex ? '50%' : 'auto',
+                    // left: index === playingIndex ? '50%' : 'auto',
+                    // transform: index === playingIndex ? 'translate(-50%, -50%)' : 'none',
+                    bottom: index === playingIndex ? '10vh' : '',
+                    left: index === playingIndex ? '5%' : '',
+                    margin:'0 auto',
+                    transition: 'all 1.3s ease-in-out',
+                    // width: index === playingIndex ? '100%' : '350px',
+                    // height: index === playingIndex ? '100%' : '200px',
+                    border: index === playingIndex ? '1px solid var(--theme-ui-colors-siteColor)' : 'inherit',
+                    boxShadow: index === playingIndex ? '2px 1px 10px 10px rgba(0, 0, 0, 0.5)' : 'inherit',
+                    // width: '80vw',
+                    // height:'60vh',
+                    // margin: index === playingIndex ? '0' : '0 auto 15px auto',
+                    zIndex: index === playingIndex ? '9999' : '1',
+                    aspectRatio: '16/9',
+                  }}
+                  light={`https://i.ytimg.com/vi/${extractVideoId(node.frontmatter.youtube.youtuber)}/hqdefault.jpg`}
                   config={{
                     file: {
                       attributes: {
@@ -63,32 +120,59 @@ const BlogList = ({ data, pageContext }) => {
                       },
                     },
                     youtube: {
-                      playerVars: { showinfo: 1, autoplay: 0, controls: 1, mute: 1, loop: 1 },
+                      playerVars: { showinfo: 0, autoplay: 1, controls: 1, mute: 0, loop: 1 },
                     },
                   }}
+                  playIcon={
+                    <div style={{display:'flex', flexDirection:'column', placeContent:'', justifyContent:'', position:'absolute', zindex:'1', top:'', fontWeight:'bold', padding:'3% 0 0 0', width:'100%', maxWidth:'25vw', height:'', border:'0px solid', borderRadius:'12px', margin:'0 auto 0 auto', opacity:'.99', textShadow:'2px 2px 2px black', color:'#fff' }}>
+                      <div className="spotlight font" style={{}}>
+                        <div className="posticons" style={{ flexDirection: 'column', margin: '0 auto' }}>
+                          <div style={{ display: 'flex', justifyContent: 'space-around', gap: '2vw', color: 'fff', }}>
+                            <ImPlay className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', fontSize: '' }} />
+                          </div>
+                          {dicPlayVideo}
+                        </div>
+                      </div>
+                    </div>}
+                    onPlay={() => handleVideoPlay(index)}
+                    onPause={handleVideoPause}
                 />
+                </div>
               ) : (
-                <div>
+                <Link className="postlink" state={showModals ? { modal: true } : {}} key={node.frontmatter.slug} to={node.frontmatter.slug}>
                   {node.frontmatter.featuredImage ? (
                     <GatsbyImage
                       image={node.frontmatter.featuredImage.childImageSharp.gatsbyImageData}
                       alt={node.frontmatter.title + " - Featured image"}
                       className="featured-image1"
                       placeholder="blurred"
-                      style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto' }}
+                      style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto', borderRadius:'var(--theme-ui-colors-borderRadius)' }}
                     />
                   ) : (
                     <StaticImage
                       className="featured-image1"
                       src="../../static/assets/default-og-image.webp"
                       alt="Default Image"
-                      style={{ position: 'relative', zIndex: '' }}
+                      style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto', borderRadius:'var(--theme-ui-colors-borderRadius)' }}
                     />
                   )}
-                </div>
+{(node.frontmatter.youtube?.youtuber && node.frontmatter.youtube.youtuber) ? (
+
+                      <div className="spotlight font" style={{border:'0px solid'}}>
+                        <div className="posticons" style={{ flexDirection: 'column', margin: '0 auto' }}>
+                          <div style={{ display: 'flex', justifyContent: 'space-around', gap: '2vw', color: 'fff', }}>
+                            <FaImage className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', fontSize: '' }} />
+                            <ImPlay className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', fontSize: '' }} />
+                            <AiOutlinePicLeft className="posticon" style={{ margin: '0 auto', width: '60%', height: '30px', }} />
+                          </div>
+                          {dicPlayMultimedia}
+                        </div>
+                      </div>
+                    ) : ("")}
+                </Link>
               )}
 
-<div className="post-content" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100%', height: '', position: 'relative', background: '', padding: '', margin: '0 auto 0 auto', textAlign: 'center', overFlow: 'hidden' }}>
+              <div className="post-content" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100%', height: '', position: 'relative', background: '', padding: '', margin: '0 auto 0 auto', textAlign: 'center', overFlow: 'hidden' }}>
 
 {showTitles ? (
   <>
@@ -110,9 +194,11 @@ const BlogList = ({ data, pageContext }) => {
   ""
 )}
               </div>
-            </Link>
-            
+
           </div>
+
+
+      
         ))}
 
         <div
