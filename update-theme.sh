fb7b8dc8c2137e6541cd3cdb349687709ff37d59
Todd <ihatespam@gmail.com>
2024-01-25 14:23:49 -0600

    Update update-theme.sh

--- update-theme.sh
+++ update-theme.sh
@@ -1,41 +1,41 @@
 # !/bin/bash
 
 # Set the URL of your central repository
-THEME_REPO_URL="https://github.com/twilightscapes/pirateplus"
+# THEME_REPO_URL="https://github.com/twilightscapes/pirateplus"
 
-# Set the branch or tag you want to pull updates from
-BRANCH_OR_TAG="main"
+# # Set the branch or tag you want to pull updates from
+# BRANCH_OR_TAG="main"
 
-# Backup user changes
-mv src user_src_backup
+# # Backup user changes
+# mv src user_src_backup
 
-# Clone the central repository
-git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
+# # Clone the central repository
+# git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
 
-# Replace the src folder
-rm -rf src
-mv tmp_theme/src .
+# # Replace the src folder
+# rm -rf src
+# mv tmp_theme/src .
 
-# Replace the gatsby-config.js file
-cp tmp_theme/gatsby-config.js .
+# # Replace the gatsby-config.js file
+# cp tmp_theme/gatsby-config.js .
 
-# Replace the gatsby-node.js file
-cp tmp_theme/gatsby-node.js .
+# # Replace the gatsby-node.js file
+# cp tmp_theme/gatsby-node.js .
 
-# Replace the netlify.toml file
-cp tmp_theme/netlify.toml .
+# # Replace the netlify.toml file
+# cp tmp_theme/netlify.toml .
 
-# Update the admin/config.yml file
-cp tmp_theme/static/admin/config.yml static/admin/
+# # Update the admin/config.yml file
+# cp tmp_theme/static/admin/config.yml static/admin/
 
-# Copy the package.json file
-cp tmp_theme/package.json .
+# # Copy the package.json file
+# cp tmp_theme/package.json .
 
-# Clean up
-rm -rf tmp_theme
+# # Clean up
+# rm -rf tmp_theme
 
-echo "PIRATE Plus updated successfully!"
+# echo "PIRATE Plus updated successfully!"
 
 
 
-# echo "No Theme Updated - Needed"
+echo "No Theme Updated - Needed"
