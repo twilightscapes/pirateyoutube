7ef20a1268f6269323663e556014b91b920b9edc
Todd <ihatespam@gmail.com>
2024-01-25 19:06:54 -0600

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -10,31 +10,36 @@ const HomePage = ({ data }) => {
   const { markdownRemark } = data;
   const { frontmatter, excerpt } = markdownRemark;
 
-  // Set the initial state directly from localStorage
-  const storedValue = localStorage.getItem("isSliderVisible");
+  // Check if localStorage is available
+  const isLocalStorageAvailable = typeof window !== "undefined" && window.localStorage;
+
+  // Set the initial state directly from localStorage if available, otherwise set to true
+  const storedValue = isLocalStorageAvailable ? localStorage.getItem("isSliderVisible") : null;
   const initialSliderVisible = storedValue ? JSON.parse(storedValue) : true;
 
   const [isSliderVisible, setIsSliderVisible] = useState(initialSliderVisible);
 
   useEffect(() => {
-    // Update isSliderVisible when it changes in localStorage
-    const handleStorageChange = () => {
-      const storedValue = localStorage.getItem("isSliderVisible");
-      try {
-        setIsSliderVisible(JSON.parse(storedValue) ?? true);
-      } catch (error) {
-        setIsSliderVisible(true);
-      }
-    };
+    if (isLocalStorageAvailable) {
+      // Update isSliderVisible when it changes in localStorage
+      const handleStorageChange = () => {
+        const storedValue = localStorage.getItem("isSliderVisible");
+        try {
+          setIsSliderVisible(JSON.parse(storedValue) ?? true);
+        } catch (error) {
+          setIsSliderVisible(true);
+        }
+      };
 
-    // Add event listener for storage change
-    window.addEventListener("storage", handleStorageChange);
+      // Add event listener for storage change
+      window.addEventListener("storage", handleStorageChange);
 
-    // Cleanup function to remove event listener
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
+      // Cleanup function to remove event listener
+      return () => {
+        window.removeEventListener("storage", handleStorageChange);
+      };
+    }
+  }, [isLocalStorageAvailable]);
 
   return (
     <Layout>
