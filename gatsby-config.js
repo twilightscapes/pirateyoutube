fee0cb145e56548892da3dc279760cd24cb4e675
Todd <ihatespam@gmail.com>
2024-01-26 10:06:46 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -457,7 +457,7 @@ module.exports = {
         lang: `en`,
         theme_color: `#111`,
         display: `standalone`,
- icon: `/static${settings.meta.icon512}`, // This path is relative to the root of the site.
+ icon: `static${settings.meta.icon512}`, // This path is relative to the root of the site.
  icon_options: {
   purpose: `any maskable`,
 },
