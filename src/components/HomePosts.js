9f49ed7f68e9643e1cb4d1935456c7724aa3b1a0
Todd <ihatespam@gmail.com>
2024-01-26 03:52:26 -0600

    Update HomePosts.js

--- src/components/HomePosts.js
+++ src/components/HomePosts.js
@@ -14,6 +14,7 @@ import { AiOutlinePicLeft } from "react-icons/ai";
 // const HomePosts = ({ isSliderVisible }) => {
 
 const HomePosts = ({ isSliderVisible }) => {
+// eslint-disable-next-line
   const [sliderVisible, setSliderVisible] = useState(false); // Change the state variable name
 
 
