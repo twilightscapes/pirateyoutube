42a92c2ea4b4d2f5af7ac16378473ff1ab3b3c1b
Todd <ihatespam@gmail.com>
2024-01-26 10:55:37 -0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,16 @@
 import React from "react"
-// import useSiteMetadata from "../hooks/SiteMetadata"
+import { Link } from "gatsby"
+import { RiArrowLeftSLine, RiBugLine } from "react-icons/ri"
+import { StaticImage } from "gatsby-plugin-image"
+// import { AiFillRobot } from "react-icons/ai"
 import Seo from "../components/seo"
 import Layout from "../components/siteLayout"
 
-function NotFound () {
-
-  return (
-
+const NotFound = () => (
   <Layout className="not-found-page">
     <Seo title="Page not found" />
 
-      <header className=""></header>
+      <header>
 
         {/* <AiFillRobot
           style={{
@@ -20,69 +20,26 @@ function NotFound () {
         textAlign:'center'
           }}
         /> */}
+ <div style={{display:'flex', justifyContent:'center', gap:'30px', width:'100vw', position:'absolute', zIndex:'1', top:'70vh', border:'0px solid'}}>
+
+<StaticImage src="../img/moose.webp" alt="Todd builds Web Apps" style={{height:'auto', position:'fixed', top:'100px', left:'0', zIndex:'-1', width:'100vw', maxHeight:'60vh',  objectFit:'cover', overflow:'', border:'0px solid red !important'}}  />
 
 
+        <h1 className="headline1" style={{fontSize:'200%'}}>Well, shit.</h1>
+        <p className="headline1" style={{fontSize:'150%', margin:'2rem'}}>
+          That wasn't supposed to happen.
+        </p>
 
-<div className="scroll-container1" style={{display:'flex', justifyContent:'start', maxWidth:'', height:'calc(100vh - 70px)', margin:'0 auto 0 auto', position:'relative', left:'0', right:'0', top:'0'}}>
-<iframe title="Pirate Frame" id="youtube2" className="blog-video1" width="100%" height="400" src="/admin/" frameBorder="0" playsInline  style={{position:'absolute', top:'0', left:'0', right:'0', zIndex:'0', width:'100%', height:'calc(100vh - 70px)', minHeight:'', border:'0px solid yellow', borderRadius:'0', padding:'0 0 0 0' }} />
-</div>
 
+     
+        <Link to="/" className="button">
+        <RiArrowLeftSLine className="icon -left" />
+        Back to Homepage
+      </Link>
+      </div>
+
+</header>
   </Layout>
-  );
-      }
+)
 
 export default NotFound
-
-
-
-// import React from "react"
-// import { Link } from "gatsby"
-// import { RiArrowLeftSLine, RiBugLine } from "react-icons/ri"
-// import { StaticImage } from "gatsby-plugin-image"
-// // import { AiFillRobot } from "react-icons/ai"
-// import Seo from "../components/seo"
-// import Layout from "../components/siteLayout"
-
-// const NotFound = () => (
-//   <Layout className="not-found-page">
-//     <Seo title="Page not found" />
-
-//       <header className="tvscreen"></header>
-
-//         {/* <AiFillRobot
-//           style={{
-//             fontSize: "160px",
-//             color: "var(--primary-color)",
-//             margin:'0 auto',
-//         textAlign:'center'
-//           }}
-//         /> */}
-//  <div style={{display:'grid', justifyContent:'center', gap:'30px', height:'100vh', width:'100vw',  border:'0px solid', background:'rgba(0, 0, 0, .8)'}}>
-
-  
-
-//  <StaticImage src="../img/moose.webp" alt="Todd builds Web Apps" style={{height:'auto', position:'fixed', top:'80px', left:'0', zIndex:'', width:'100vw', maxHeight:'60%',  objectFit:'contain', overflow:'',}}  />
-
-
-// {/* <h1 className="headline1" style={{fontSize:'200%'}}>Well, Darn.</h1> */}
-// <p className="headline1" style={{fontSize:'', margin:'2rem auto'}}>
-  
-// </p>
-
-// <div style={{display:'flex',}}>
-//         <Link to="/" className="button">
-//         <RiArrowLeftSLine className="icon -left" />
-//         Back to Homepage
-//       </Link>
-//       <Link to="/contact" className="button">
-//         Report this &nbsp;<RiBugLine className="icon -right" />
-//       </Link></div>
-
-      
-//       </div>
-
-
-//   </Layout>
-// )
-
-// export default NotFound
