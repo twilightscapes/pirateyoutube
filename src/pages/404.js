fd62b73f3a1c83b4c9186967669186fedeb99c05
Todd <ihatespam@gmail.com>
2024-02-06 06:04:49 -0600

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,7 +14,7 @@ const NotFound = () => (
 
 
 
-<div style={{display:'grid', placeContent:'center', height:'100%', position:'absolute', top:'-15vh', }}>
+<div style={{display:'grid', placeContent:'center', height:'90dvh', position:'absolute', top:'-15vh', }}>
 <StaticImage className="panel" src="../img/moose.webp" alt="Todd builds Web Apps" style={{  border:'0px solid red !important'}}  />
 
 <Link to="/" className="button" style={{maxWidth:'300px', margin:'0 auto'}}>
