7ca781c6059be7dd691aaf04f1593892d9a3dea0
Todd <ihatespam@gmail.com>
2024-02-15 01:48:20 -0600

    app

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -2,38 +2,24 @@ import React, { useState, useRef, useEffect } from "react";
 import ReactPlayer from 'react-player/lazy';
 import { ImYoutube2 } from "react-icons/im";
 import { FaTwitch, FaFacebookSquare } from "react-icons/fa";
-// import PageMenu from "../components/PageMenu"
+import { Link } from "gatsby";
 
 const VideoPlayer = () => {
   const inputElement = useRef(null);
   const playerRef = useRef(null);
   const [youtubelink, setYoutubelink] = useState("");
+  const [showShareDialog, setShowShareDialog] = useState(false);
 
   useEffect(() => {
-    const inputRef = inputElement.current;
-    inputRef.onfocus = () => {
-      document.body.scrollTop = 0;
-    };
-    inputRef.onblur = () => {
-      document.body.scrollTop = 0;
+    const fillFormFromClipboard = async () => {
+      try {
+        const clipboardText = await navigator.clipboard.readText();
+        setYoutubelink(clipboardText);
+      } catch (error) {
+        console.error("Error reading clipboard:", error);
+      }
     };
 
-    return () => {
-      inputRef.onfocus = null;
-      inputRef.onblur = null;
-    };
-  }, []);
-
-  const fillFormFromClipboard = async () => {
-    try {
-      const clipboardText = await navigator.clipboard.readText();
-      setYoutubelink(clipboardText);
-    } catch (error) {
-      console.error("Error reading clipboard:", error);
-    }
-  };
-
-  useEffect(() => {
     fillFormFromClipboard();
   }, []);
 
@@ -45,6 +31,28 @@ const VideoPlayer = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Add any additional logic you need on form submission
+  };
+
+  function isRunningStandalone() {
+    return (window.matchMedia('(display-mode: standalone)').matches);
+  }
+
+  const handleShareButtonClick = () => {
+    if (navigator.share) { 
+      navigator.share({
+        title: 'WebShare API Demo',
+        url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+      }).then(() => {
+        console.log('Thanks for sharing!');
+      })
+      .catch(console.error);
+    } else {
+      setShowShareDialog(true);
+    }
+  };
+
+  const closeShareDialog = () => {
+    setShowShareDialog(false);
   };
 
   return (
@@ -68,19 +76,73 @@ const VideoPlayer = () => {
             },
           }}
         />
-        {/* <PageMenu /> */}
         <div className="form-container controller font" style={{position:'relative', zIndex:'2', top:'0', marginTop:'0', height:'', padding:'2vh 2%', width:'100vw', background:'var(--theme-ui-colors-headerColor)'}}>
           <div style={{ maxWidth: '800px', margin: '0 auto' }}>
             <form className="youtubeform frontdrop" onSubmit={handleSubmit}id="youtubeform" name="youtubeform">
-              <a title="Open YouTube" aria-label="Open YouTube" href="https://youtube.com">
-                <ImYoutube2 style={{ fontSize: '50px' }} />
-              </a>
-              <a title="Open Facebook" aria-label="Open Facebook" href="https://www.facebook.com/watch/">
-                <FaFacebookSquare style={{ fontSize: '30px' }} />
-              </a>
-              <a title="Open Twitch" aria-label="Open Twitch" href="https://www.twitch.tv/directory">
-                <FaTwitch style={{ fontSize: '30px' }} />
-              </a>
+              {isRunningStandalone() ? (
+                <>
+                  <a title="Open YouTube" aria-label="Open YouTube" href="https://youtube.com">
+                    <ImYoutube2 style={{ fontSize: '50px' }} />
+                  </a>
+                  <a title="Open Facebook" aria-label="Open Facebook" href="https://www.facebook.com/watch/">
+                    <FaFacebookSquare style={{ fontSize: '30px' }} />
+                  </a>
+                  <a title="Open Twitch" aria-label="Open Twitch" href="https://www.twitch.tv/directory">
+                    <FaTwitch style={{ fontSize: '30px' }} />
+                  </a>
+                </>
+              ) : (
+                <>
+                  <div className="share-dialog" style={{ display: showShareDialog ? 'block' : 'none' }}>
+                    <h3 className="dialog-title">Install PIRATE</h3>
+                    <button className="close-button" onClick={closeShareDialog}>Close</button>
+                    {/* <div className="targets">
+                      <a className="button">
+                        <svg>
+                          <use href="#facebook"></use>
+                        </svg>
+                        <span>Facebook</span>
+                      </a>
+                      <a className="button">
+                        <svg>
+                          <use href="#twitter"></use>
+                        </svg>
+                        <span>Twitter</span>
+                      </a>
+                      <a className="button">
+                        <svg>
+                          <use href="#linkedin"></use>
+                        </svg>
+                        <span>LinkedIn</span>
+                      </a>
+                      <a className="button">
+                        <svg>
+                          <use href="#email"></use>
+                        </svg>
+                        <span>Email</span>
+                      </a>
+                    </div> */}
+                    <div className="link">
+                      <div className="pen-url">https://pirateweb.org</div>
+                      <button className="copy-link">Copy Link</button>
+                    </div>
+                  </div>
+
+                  
+                  <button style={{ display: "flex", justifyContent: "center", padding: "10px .3vw", maxWidth: "", margin: "0 auto", textAlign:'center', fontSize:'14px', fontWeight:'light', textShadow:'0 1px 0 #000' }} className="button print" type="button" title="Add To Home Screen To Install PIRATE" onClick={handleShareButtonClick}>
+                    <svg style={{maxWidth:'50px'}}>
+                      <use href="#share-icon"></use>
+                    </svg>
+                    <div style={{ display: "flex", justifyContent: "center", padding: "10px .3vw", maxWidth: "", margin: "0 auto", textAlign:'center', fontSize:'14px', fontWeight:'light', textShadow:'0 1px 0 #000' }}>Add To Home Screen To Install PIRATE</div>
+                  </button>
+                  <svg className="hidden">
+                    <defs>
+                      <symbol id="share-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></symbol>
+                      {/* Define other SVG symbols here */}
+                    </defs>
+                  </svg>
+                </>
+              )}
               <input
                 ref={inputElement}
                 id="youtubelink-input"
@@ -89,8 +151,8 @@ const VideoPlayer = () => {
                 value={youtubelink}
                 onChange={handleInputChange}
                 style={{ padding: '.5vh 1vw', width:'100%', minWidth: '', outline: '1px solid #333', borderRadius: 'var(--theme-ui-colors-borderRadius)', color: 'var(--theme-ui-colors-siteColor)', fontSize:'clamp(.8rem,1.5vw,2rem)' }}
-              placeholder="Paste Video Link"
-              className="youtubelinker"
+                placeholder="Paste Video Link"
+                className="youtubelinker"
               />
               <button type="reset" onClick={() => setYoutubelink("")} style={{ color: '', fontSize:'clamp(.8rem,1.5vw,2rem)', fontWeight: 'bold', textAlign: 'left', width: '', margin: '5px 15px 0 0' }}>
                 Reset
--- src/pages/video.js
+++ src/pages/video.js
@@ -2,10 +2,12 @@ import React, { useState, useRef, useEffect } from "react";
 import ReactPlayer from 'react-player/lazy';
 import { ImYoutube2 } from "react-icons/im";
 import { FaTwitch, FaFacebookSquare } from "react-icons/fa";
+import { Link } from "gatsby";
 import PageMenu from "../components/PageMenu"
 import Layout from "../components/siteLayout"
 import Seo from "../components/seo";
 import { Helmet } from "react-helmet";
+
 const Video = () => {
   const inputElement = useRef(null);
   const playerRef = useRef(null);
@@ -44,77 +46,84 @@ const Video = () => {
     event.preventDefault();
   };
 
+  function isRunningStandalone() {
+    return (window.matchMedia('(display-mode: standalone)').matches);
+  }
+
   useEffect(() => {
     fillFormFromClipboard();
   }, []); // Empty dependency array to run the effect only once
 
   return (
     <>
-    <Layout>
-<Helmet>
-    <body id="body" className="youtube"/>
-  </Helmet>
-<Seo
-    title="AdFree Video Player"
-    description="Adfree Video Player"
-  />
-      <div className='player-wrapper' style={{}}>
-      <PageMenu />
-        <ReactPlayer
-          ref={playerRef}
-          allow="web-share"
-          style={{
-            position: 'relative', top: '0', margin: '0 auto 0 auto', zIndex: '1', overflow: 'hidden', width: '100vw', minHeight: '', height: '100%', background: 'transparent'
-          }}
-          width="100%"
-          height="100%"
-          url={youtubelink}
-          playing={true}
-          controls={true}
-          playsinline
-          config={{
-            youtube: {
-              playerVars: { showinfo: false, autoplay: false, controls: true, start: "0", end: null, mute: false, loop: false }
-            },
-          }}
+      <Layout>
+        <Helmet>
+          <body id="body" className="youtube"/>
+        </Helmet>
+        <Seo
+          title="AdFree Video Player"
+          description="Adfree Video Player"
         />
+        <div className='player-wrapper' style={{}}>
+          <PageMenu />
+          <ReactPlayer
+            ref={playerRef}
+            allow="web-share"
+            style={{
+              position: 'relative', top: '0', margin: '0 auto 0 auto', zIndex: '1', overflow: 'hidden', width: '100vw', minHeight: '', height: '100%', background: 'transparent'
+            }}
+            width="100%"
+            height="100%"
+            url={youtubelink}
+            playing={true}
+            controls={true}
+            playsinline
+            config={{
+              youtube: {
+                playerVars: { showinfo: false, autoplay: false, controls: true, start: "0", end: null, mute: false, loop: false }
+              },
+            }}
+          />
           
           <div className="form-container controller font" style={{position:'relative', zIndex:'2', top:'0', marginTop:'0', height:'', padding:'2vh 2%', width:'100vw', background:'var(--theme-ui-colors-headerColor)'}}>
-          <div style={{ maxWidth: '800px', margin: '0 auto' }}>
-            <form className="youtubeform frontdrop" onSubmit={handleSubmit} id="youtubeform" name="youtubeform">
-            <a title="Open YouTube" aria-label="Open YouTube" href="https://youtube.com">
-                <ImYoutube2 style={{ fontSize: '50px' }} />
-              </a>
-              <a title="Open Facebook" aria-label="Open Facebook" href="https://www.facebook.com/watch/">
-                <FaFacebookSquare style={{ fontSize: '30px' }} />
-              </a>
-              <a title="Open Twitch" aria-label="Open Twitch" href="https://www.twitch.tv/directory">
-                <FaTwitch style={{ fontSize: '30px' }} />
-              </a>
-              <input
-                ref={inputElement}
-                id="youtubelink-input"
-                type="text"
-                name="youtubelink"
-                value={youtubelink}
-                onChange={handleInputChange}
-                style={{ padding: '.5vh 1vw', width:'100%', minWidth: '', outline: '1px solid #333', borderRadius: 'var(--theme-ui-colors-borderRadius)', color: 'var(--theme-ui-colors-siteColor)', fontSize:'clamp(.8rem,1.5vw,2rem)' }}
-              placeholder="Paste Video Link"
-              className="youtubelinker"
-              />
-              <button type="reset" onClick={() => setYoutubelink("")} style={{ color: '', fontSize:'clamp(.8rem,1.5vw,2rem)', fontWeight: 'bold', textAlign: 'left', width: '', margin: '5px 15px 0 0' }}>
-                Reset
-              </button>
-            </form>
+            <div style={{ maxWidth: '800px', margin: '0 auto' }}>
+              <form className="youtubeform frontdrop" onSubmit={handleSubmit} id="youtubeform" name="youtubeform">
+
+                {isRunningStandalone() ? (
+                  <>
+                    <a title="Open YouTube" aria-label="Open YouTube" href="https://youtube.com">
+                      <ImYoutube2 style={{ fontSize: '50px' }} />
+                    </a>
+                    <a title="Open Facebook" aria-label="Open Facebook" href="https://www.facebook.com/watch/">
+                      <FaFacebookSquare style={{ fontSize: '30px' }} />
+                    </a>
+                    <a title="Open Twitch" aria-label="Open Twitch" href="https://www.twitch.tv/directory">
+                      <FaTwitch style={{ fontSize: '30px' }} />
+                    </a>
+                  </>
+                ) : (
+                  <Link to="/install" state={{ modal: true }} className="button print" style={{ display: "flex", justifyContent: "center", padding: ".5vh .5vw", maxWidth: "", margin: "0 auto", textAlign:'center', fontSize:'16px' }}>Add To Home Screen To Install Pirate</Link>
+                )}
+
+                <input
+                  ref={inputElement}
+                  id="youtubelink-input"
+                  type="text"
+                  name="youtubelink"
+                  value={youtubelink}
+                  onChange={handleInputChange}
+                  style={{ padding: '.5vh 1vw', width:'100%', minWidth: '', outline: '1px solid #333', borderRadius: 'var(--theme-ui-colors-borderRadius)', color: 'var(--theme-ui-colors-siteColor)', fontSize:'clamp(.8rem,1.5vw,2rem)' }}
+                  placeholder="Paste Video Link"
+                  className="youtubelinker"
+                />
+                <button type="reset" onClick={() => setYoutubelink("")} style={{ color: '', fontSize:'clamp(.8rem,1.5vw,2rem)', fontWeight: 'bold', textAlign: 'left', width: '', margin: '5px 15px 0 0' }}>
+                  Reset
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      
-
-
-
-
-</Layout>
+      </Layout>
     </>
   );
 };
