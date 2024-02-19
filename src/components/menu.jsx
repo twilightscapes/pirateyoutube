ba355197ae5ac14847cf40680686faee1ed40993
Todd <ihatespam@gmail.com>
2024-02-18 20:35:24 -0600

    Update menu.jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "gatsby";
-import Menu from "../../static/data/menu.json";
+import MenuSocial from "../../static/data/menu-social.json";
+import MenuDefault from "../../static/data/menu.json";
 import useSiteMetadata from "../hooks/SiteMetadata";
 
 export function MenuInc() {
   const { proOptions } = useSiteMetadata();
-  const { showModals } = proOptions
+  const { showModals } = proOptions;
+  const bodyClass = document.body.classList.contains('social') ? 'social' : 'default';  // Check body class
 
-  const newMenu = Menu.menuItems.map((menu) => (
+  const menuItems = bodyClass === 'social' ? MenuSocial.menuItems : MenuDefault.menuItems;
+
+  const menuLinks = menuItems.map((menu) => (
     <Link
       key={menu.menutitle}
       aria-label={menu.menutitle}
@@ -15,7 +19,6 @@ export function MenuInc() {
       title={menu.menutitle}
       className="navbar-item"
       state={showModals ? { modal: true } : {}}
-      // state={{modal: true}}
     >
       {menu.menutitle}
     </Link>
@@ -23,7 +26,7 @@ export function MenuInc() {
 
   return (
     <>
-      {newMenu.map((menuItem, index) => (
+      {menuLinks.map((menuItem, index) => (
         <li key={index}>{menuItem}</li>
       ))}
     </>
