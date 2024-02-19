ed57aed6b0662868e1afee14e4ef63d4d2be6a3f
Todd <ihatespam@gmail.com>
2024-02-18 22:55:46 -0600

    wc!

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -8,55 +8,23 @@ import PageMenu from "../components/PageMenu";
 const VideoPlayer = ({ location }) => {
   const queryParams = new URLSearchParams(location.search);
   const videoUrlParam = queryParams.get('video');
-  const { featureOptions, proOptions } = useSiteMetadata();
-  const { showBranding } = proOptions;
-  const { showNav } = featureOptions;
   const inputElement = useRef(null);
   const playerRef = useRef(null);
   const [youtubelink, setYoutubelink] = useState(videoUrlParam || "");
-  const [copied, setCopied] = useState(false);
+  const [showShareDialog, setShowShareDialog] = useState(false);
 
-  useEffect(() => {
-    const fillFormFromUrlParam = async () => {
-      try {
-        // Check if the document has focus
-        if (!document.hasFocus()) {
-          // throw new Error("Document is not focused. Please interact with the page.");
-        }
-  
-        if (isValidURL(videoUrlParam)) {
-          // Only update the input value if the URL parameter is present and the input field is empty
-          if (!youtubelink) {
-            setYoutubelink(videoUrlParam);
-          }
-        } else {
-          // console.error("Invalid URL:", videoUrlParam);
-          // You can handle this case accordingly, such as displaying a message to the user
-        }
-      } catch (error) {
-        // console.error("Error reading URL parameter:", error.message);
-        // You can handle the error here, e.g., display a message to the user
-      }
-    };
-    
-    fillFormFromUrlParam();
-  }, [videoUrlParam, youtubelink]); // Include videoUrlParam and youtubelink in dependency array
-  
-  
-  
-  
-  
-  
-  
-  
+  const { featureOptions, proOptions } = useSiteMetadata();
+  const { showBranding } = proOptions;
+  const { showNav } = featureOptions;
 
   const handleInputChange = (event) => {
     const { value } = event.target;
-    if (isValidURL(value)) {
-      setYoutubelink(value);
-      updateQueryString(value);
-    } else {
-      console.error("Invalid URL:", value);
+    setYoutubelink(value);
+    updateQueryString(value);
+
+    const pirateVideoElement = document.getElementById('VideoPlayer');
+    if (pirateVideoElement) {
+      pirateVideoElement.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
@@ -74,18 +42,6 @@ const VideoPlayer = ({ location }) => {
     window.history.pushState({}, '', newUrl);
   };
 
-  const copyToClipboard = () => {
-    if (typeof window !== 'undefined') {
-      navigator.clipboard.writeText(window.location.href)
-        .then(() => {
-          setCopied(true);
-          setTimeout(() => setCopied(false), 2000); // Reset 'copied' state after 2 seconds
-        })
-        .catch((error) => console.error("Error copying to clipboard:", error));
-    }
-  };
-
-
   function isRunningStandalone() {
     if (typeof window !== 'undefined') {
       return window.matchMedia('(display-mode: standalone)').matches;
@@ -93,29 +49,61 @@ const VideoPlayer = ({ location }) => {
     return false;
   }
 
+  const handleShareButtonClick = () => {
+    if (typeof window !== 'undefined') {
+      if (navigator.share) { 
+        navigator.share({
+          title: 'PIRATE',
+          url: window.location.href
+        }).then(() => {
+          console.log('Thanks for being a Pirate!');
+        })
+        .catch(console.error);
+      } else {
+        setShowShareDialog(true);
+      }
+    }
+  };
+
+  const closeShareDialog = () => {
+    setShowShareDialog(false);
+  };
+
+  const copyToClipboard = () => {
+    if (typeof window !== 'undefined') {
+      navigator.clipboard.writeText(window.location.href);
+    }
+  };
+
+  const handleCopyAndShareButtonClick = () => {
+    copyToClipboard();
+    handleShareButtonClick();
+  };
+
   return (
     <>
-      <div id="piratevideo" className='player-wrapper' style={{ display: 'grid', placeContent: '', width: '100vw', transition: 'all 1s ease-in-out' }}>
-        {/* Form Container */}
-        <div className="form-container controller font" style={{ position: 'relative', zIndex: '3', top: '0', height: 'auto', width: '100vw', margin: '0 auto', marginTop: showNav ? '0' : '0', transition: 'all 1s ease-in-out', background: 'var(--theme-ui-colors-headerColor)' }}>
-          <div style={{ maxWidth: '800px', margin: '0 auto', padding:'2.5vh 1vw 0 1vw', }}>
+      <div id="piratevideo" className='player-wrapper' style={{ display:'grid', placeContent:'', width:'100vw', transition: 'all 1s ease-in-out'}}>
+
+        <div className="form-container controller font" style={{position:'relative', zIndex:'3', top:'0', height:'auto', width:'100vw', margin:'0 auto', marginTop: showNav ? '0' : '0', transition: 'all 1s ease-in-out', background:'var(--theme-ui-colors-headerColor)'}}>
+          <div style={{ maxWidth: '800px', margin: '0 auto', paddingTop:'1.5vh' }}>
             <form className="youtubeform frontdrop" onSubmit={handleSubmit} id="youtubeform" name="youtubeform">
-              {/* Video Platform Links */}
+
               {isRunningStandalone() ? (
                 <>
                   <a title="Open YouTube" aria-label="Open YouTube" href="https://youtube.com">
-                    <ImYoutube2 style={{ fontSize: '50px', opacity:'.5' }} />
+                    <ImYoutube2 style={{ fontSize: '50px' }} />
                   </a>
                   <a title="Open Facebook" aria-label="Open Facebook" href="https://www.facebook.com/watch/">
-                    <FaFacebookSquare style={{ fontSize: '30px', opacity:'.5' }} />
+                    <FaFacebookSquare style={{ fontSize: '30px' }} />
                   </a>
                   <a title="Open Twitch" aria-label="Open Twitch" href="https://www.twitch.tv/directory">
-                    <FaTwitch style={{ fontSize: '30px', opacity:'.5' }} />
+                    <FaTwitch style={{ fontSize: '30px' }} />
                   </a>
                 </>
               ) : (
-                ""
+                <></>
               )}
+
               <input
                 ref={inputElement}
                 id="youtubelink-input"
@@ -123,43 +111,38 @@ const VideoPlayer = ({ location }) => {
                 name="youtubelink"
                 value={youtubelink}
                 onChange={handleInputChange}
-                style={{ padding: '.5vh 1vw', width: '100%', maxWidth: '800px', fontSize: 'clamp(.8rem,1.5vw,2rem)', transition: 'all 1s ease-in-out' }}
-                placeholder="Paste Link To Video"
+                style={{ padding: '.5vh 1vw', width:'100%', maxWidth: '800px', fontSize:'clamp(.8rem,1.5vw,2rem)', transition: 'all 1s ease-in-out' }}
+                placeholder="Paste Video Link"
                 className="youtubelinker"
-                aria-label="Paste Link To Video"
+                aria-label="Paste Video Link"
               />
-              <button aria-label="Reset" type="reset" onClick={handleReset} disabled={!youtubelink} style={{ color: '', fontSize: 'clamp(.8rem,1.5vw,2rem)', fontWeight: 'bold', textAlign: 'left', width: '40px', margin: '5px 15px 0 0' }}>
+              <button aria-label="Reset" type="reset" onClick={handleReset} style={{ color: '', fontSize:'clamp(.8rem,1.5vw,2rem)', fontWeight: 'bold', textAlign: 'left', width: '', margin: '5px 15px 0 0' }}>
                 Reset
               </button>
-              <button aria-label="Copy Link" onClick={copyToClipboard} disabled={!youtubelink} style={{ display: "flex", gap: '.5vw', justifyContent: "center", padding: ".5vh .8vw", width:'80px', maxHeight: "", margin: "0 auto", textAlign: 'center', fontSize: '14px', fontWeight: 'light', textShadow: '0 1px 0 #000' }} className="button font print">
-                <svg style={{ maxWidth: '30px', maxHeight: '30px' }}>
+
+              <button aria-label="Copy Link" onClick={handleCopyAndShareButtonClick} style={{ display: "flex", gap:'.5vw', justifyContent: "center", padding: ".5vh .8vw", maxHeight:"", margin: "0 auto", textAlign:'center', fontSize:'14px', fontWeight:'light', textShadow:'0 1px 0 #000' }} className="button font print">
+                <svg style={{maxWidth:'30px', maxHeight:'30px'}}>
                   <use href="#share-icon"></use>
-                </svg>   {copied ? 'Copied Link' : 'Copy Link'}
+                </svg>   Copy Link
               </button>
+
             </form>
           </div>
         </div>
 
-        {/* Hidden SVG */}
         <svg className="hidden">
           <defs>
             <symbol id="share-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></symbol>
           </defs>
         </svg>
 
-        {/* Page Menu */}
-        {showBranding ? (
-          <PageMenu />
-        ) : (
-          ""
-        )}
+        {showBranding ? <PageMenu /> : null}
 
-        {/* ReactPlayer */}
         <ReactPlayer
           ref={playerRef}
           allow="web-share"
           style={{
-            position: 'relative', top: '0', margin: '0 auto 0 auto', zIndex: '1', overflow: 'hidden', width: '100vw', minHeight: '', height: '100%', background: 'transparent',
+            position: 'relative', top: '0', margin: '0 auto 0 auto', zIndex: '1',  overflow: 'hidden', width: '100vw', minHeight: '', height: '100%', background: 'transparent',
             transition: 'all 1s ease-in-out',
           }}
           width="100%"
@@ -180,10 +163,3 @@ const VideoPlayer = ({ location }) => {
 };
 
 export default VideoPlayer;
-
-// Function to validate URL (You can use a library like 'valid-url' for more comprehensive validation)
-const isValidURL = (url) => {
-  // Regular expression for URL validation
-  const urlPattern = /^(ftp|http|https):\/\/[^ "]+$/;
-  return urlPattern.test(url);
-};
