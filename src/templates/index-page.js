ae27f0bd68094da775f0de35242a70fa1e044617
Todd <ihatespam@gmail.com>
2024-02-26 02:46:52 -0600

    test1

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -18,46 +18,41 @@ const VideoPage = ({ location }) => {
   );
 };
         // Function to check if the app is running in standalone mode
-      //   function isRunningStandalone() {
-      //     if (typeof window !== 'undefined') {
-      //         return window.matchMedia('(display-mode: standalone)').matches;
-      //     }
-      //     return false;
-      // }
-
-
-      const SeoWrapper = ({ location }) => {
-        const queryParams = new URLSearchParams(location.search);
-        const videoUrlParam = queryParams.get('video');
-        const seoTitle = queryParams.get('seoTitle') || "☠ Pirate YouTube | Play Video ▶ ";
-      
-        const extractVideoId = (url) => {
-          if (!url) {
-            return null;
+        function isRunningStandalone() {
+          if (typeof window !== 'undefined') {
+              return window.matchMedia('(display-mode: standalone)').matches;
           }
-          const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-          const match = url.match(regExp);
-          const videoId = match ? match[1] : null;
-          return videoId;
-        };
-      
-        const videoId = extractVideoId(videoUrlParam);
-      
-        // Define the fallback image URL
-        const fallbackImageUrl = 'https://pirateyoutube.com/assets/default-og-image.webp';
-      
-        // Construct the image URL based on the video ID
-        const imageUrl = videoId ? `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg` : fallbackImageUrl;
-      
-        return (
-          <Seo
-            title={seoTitle}
-            description="Pirate YouTube - video on your terms"
-            image={imageUrl}
-          />
-        );
-      };
-      
-      
+          return false;
+      }
+
+
+const SeoWrapper = ({ location }) => {
+  const queryParams = new URLSearchParams(location.search);
+  const videoUrlParam = queryParams.get('video');
+  const seoTitle = queryParams.get('seoTitle') || "Play Video "; // Use the seoTitle from query params or default value
+
+  // Function to extract video ID from YouTube URL
+  const extractVideoId = (url) => {
+    if (!url) {
+      return null;
+    }
+    /* eslint-disable no-useless-escape */
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] : null;
+    return videoId;
+  };
+  /* eslint-enable no-useless-escape */
+
+  const videoId = extractVideoId(videoUrlParam);
+
+  return (
+    <Seo
+      title={seoTitle}
+      description="Pirate: revolutionizing the web"
+      image={videoId ? `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg` : 'https://pirateyoutube.com/assets/default-og-image.webp'}
+    />
+  );
+};
 
 export default VideoPage;
