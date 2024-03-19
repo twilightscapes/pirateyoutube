0c57e3cb349dccdc31e1adc9e88d3f8511cd418d
Todd <ihatespam@gmail.com>
2024-03-19 04:17:46 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -114,9 +114,74 @@ const HomePage = ({ location }) => {
       </div>
 
 ) : (
+
 ""
 
   )}
+
+
+
+{isRunningStandalone() ? (
+  <div
+  className="menusnapp"
+  style={{
+    position: "absolute",
+    zIndex: "0",
+    top: "40px",
+    gap: "0",
+    padding: "2vh 2vw",
+    alignItems: "center",
+    // display: isMenuOpen ? "block" : "none",
+    display: "",
+    background: "var(--theme-ui-colors-headerBackground)",
+    backgroundColor: "#222",
+    width: "100dvw",
+  }}
+>
+  <div id="" className="flexbutt font" style={{ display: "", gap: "3vh", justifyContent: "center", alignItems: "center", margin: "0 0", padding: "0", position: "relative", minWidth: "80vw" }}>
+    <div style={{ minWidth: "25vw", maxHeight: "15vh", textAlign: "center", color: "#fff" }}>
+      <PirateLogo style={{ minWidth: "", maxHeight: "15vh", position: "", top: "", left: "" }} />
+      the web revolution
+      <br /><br /><br />
+        <span style={{ margin: "2vh auto", fontSize: "160%" }}>Install Now Free!</span>
+        <br /><br /><br />
+    </div>
+    <div className="flexcheek mob2 print" style={{ position: "", top: "", minWidth: "25vw", overflow: "", marginBottom: "", paddingTop: "", borderRadius: "var(--theme-ui-colors-borderRadius)" }}>
+      <div className="nameblock font" style={{ margin: "0 auto 0 auto", padding: "0 0 0 0", alignContent: "center", display: "grid", textAlign: "center", justifyContent: "center", verticalAlign: "center", color: "#fff", paddingTop: "", fontSize: "clamp(1rem, 1.4vw, 3.2rem)", background: "rgba(0,0,0,0.50)", backdropFilter: "blur(8px)", border: "10px double var(--theme-ui-colors-buttonHoverBg)", borderRadius: "var(--theme-ui-colors-borderRadius)", textShadow: "0 2px 0px #000", maxWidth: "" }}>
+        <br />
+        <span style={{ margin: "2vh auto", fontSize: "160%" }}>About PIRATE</span>
+        <br />
+        A web revolution is coming
+        <br /><br />
+        And it's completely FREE!
+        <br />
+        <a href="https://pirateweb.org/about" className="button print" style={{ display: "flex", justifyContent: "center", padding: "1vh .5vw", maxWidth: "250px", margin: "30px auto", border:'1px solid' }}>About PIRATE</a>
+      </div>
+    </div>
+    <div className="flexcheek mob2 print" style={{ position: "", top: "", minWidth: "25vw", overflow: "", marginBottom: "", paddingTop: "", borderRadius: "var(--theme-ui-colors-borderRadius)" }}>
+      <div className="nameblock font" style={{ margin: "0 auto 0 auto", padding: "0 0 0 0", alignContent: "center", display: "grid", textAlign: "center", justifyContent: "center", verticalAlign: "center", color: "#fff", paddingTop: "", fontSize: "clamp(1rem, 1.4vw, 3.2rem)", background: "rgba(0,0,0,0.50)", backdropFilter: "blur(8px)", border: "10px double var(--theme-ui-colors-buttonHoverBg)", borderRadius: "var(--theme-ui-colors-borderRadius)", textShadow: "0 2px 0px #000", maxWidth: "" }}>
+        <br />
+        <span style={{ margin: "2vh auto", fontSize: "160%" }}>Get PIRATE</span>
+        
+        
+        
+        Get the full version free!
+        <br /><br />
+        Web, Social &amp; Video Media
+        <br />
+        Combined into your own app
+      
+        <a href="https://pirateweb.org/install" className="button print" style={{ display: "flex", justifyContent: "center", padding: "1vh .5vw", maxWidth: "250px", margin: "30px auto", border:'1px solid ' }}>Become a PIRATE!</a>
+        
+      </div>
+    </div>
+  </div>
+</div>
+) : (
+""
+  )}
+
+
       </div>
 
     </Layout>
@@ -126,7 +191,7 @@ const HomePage = ({ location }) => {
 const SeoWrapper = ({ location }) => {
   const queryParams = new URLSearchParams(location.search);
   const videoUrlParam = queryParams.get('video');
-  const seoTitleParam = queryParams.get('seoTitle') || "☠ Pirate YouTube | Play Video ▶ ";
+  const seoTitleParam = queryParams.get('seoTitle') || "☠ Pirate Video | Play ▶ ";
   const customImageParam = queryParams.get('customImage'); 
 
   // Function to extract video ID from YouTube URL
