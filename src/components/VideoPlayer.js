ff338c0aa45d6d78695d6fc632e4e77eaf810d32
Todd <ihatespam@gmail.com>
2024-03-18 22:04:46 -0500

    test

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import ReactPlayer from 'react-player/lazy';
 import { TfiYoutube } from "react-icons/tfi";
-import { FaTwitch, FaFacebookSquare } from "react-icons/fa";
+import { FaTwitch, FaFacebookSquare, FaVimeo, FaDailymotion } from "react-icons/fa";
+import { ImSoundcloud2 } from "react-icons/im";
 import useSiteMetadata from "../hooks/SiteMetadata";
 import PageMenu from "../components/PageMenu";
 
@@ -358,7 +359,7 @@ const handleAutoplayChange = (event) => {
             if (typeof window !== 'undefined') {
                 return window.matchMedia('(display-mode: standalone)').matches;
             }
-            return false;
+            return ;
         }
 
     // Check if a video is active
@@ -388,7 +389,7 @@ const handleAutoplayChange = (event) => {
 
 
 
-            {!showPro ? (
+            {isRunningStandalone() ? (
 
 <div className="font" style={{ position: 'relative', zIndex: '3', top: '0', width: '100vw', margin: '0 auto', transition: 'all .4s ease-in-out', marginTop: showNav ? '0' : '0',
 //  height: hideEditor ? '0' : '50px', 
@@ -675,8 +676,13 @@ const handleAutoplayChange = (event) => {
 
 </div>
 
-                            {isRunningStandalone() && (
-                            <div style={{position:'absolute', left:'0', top:'50vh', zIndex:'2', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', gap:'2vh', width:'55px',padding:'3px 10px', background:'rgba(0,0,0,.2)', outline:'1px solid #333', borderRadius:'var(--theme-ui-colors-borderRadius)'}}>
+
+                        
+                    
+                    </form>
+
+                    {isRunningStandalone() && (
+                            <div style={{position:'fixed', left:'', bottom:'1vh', zIndex:'2', display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center', gap:'1.5vw', width:'100vw',padding:'3px 10px', background:'rgba(0,0,0,.2)', outline:'1px solid #333', borderRadius:'var(--theme-ui-colors-borderRadius)'}}>
                                     <a title="Open YouTube" aria-label="Open YouTube" href="https://youtube.com">
                                         <TfiYoutube style={{ fontSize: '30px', opacity:'.8' }} />
                                     </a>
@@ -686,11 +692,22 @@ const handleAutoplayChange = (event) => {
                                     <a title="Open Twitch" aria-label="Open Twitch" href="https://www.twitch.tv/directory">
                                         <FaTwitch style={{ fontSize: '30px', opacity:'.8' }} />
                                     </a>
+                                    <a title="Open Sound CLoud" aria-label="Open Sound Cloud" href="https://soundcloud.com/discover">
+                                        <ImSoundcloud2 style={{ fontSize: '30px', opacity:'.8' }} />
+                                    </a>
+                                    <a title="Open Vimeo" aria-label="Open Vimeo" href="https://vimeo.com/watch">
+                                        <FaVimeo style={{ fontSize: '30px', opacity:'.8' }} />
+                                    </a>
+                                    <a title="Open Daily Motion" aria-label="Open Daily Motion" href="https://www.dailymotion.com/">
+                                        <FaDailymotion style={{ fontSize: '30px', opacity:'.8' }} />
+                                    </a>
+
+                                    
+                                    
+                                    
                                 </div>
                              )}
-                        
-                    
-                    </form>
+
 
                     </div>
 
@@ -701,7 +718,7 @@ const handleAutoplayChange = (event) => {
     ) : (
 
 
-<div className="font public" style={{display: hideEditor ? 'flex' : 'flex', position: 'relative', zIndex: '3', top: '0', width: '100vw', margin: '0 auto', marginTop: showNav ? '0' : '', transition: 'all .4s ease-in-out', 
+<div className="font public1" style={{display: hideEditor ? 'flex' : 'flex', position: 'relative', zIndex: '3', top: '0', width: '100vw', margin: '0 auto', marginTop: showNav ? '0' : '', transition: 'all .4s ease-in-out', 
 // height: hideEditor ? '0' : '50px', 
 // background: 'var(--theme-ui-colors-headerColor)',
  }}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/siteLayout";
 import Seo from "../components/seo";
 import VideoPlayer from "../components/VideoPlayer";
 import { Helmet } from "react-helmet";
-
+import PirateLogo from "../img/logo.svg";
 const HomePage = ({ location }) => {
   return (
     <Layout>
@@ -13,7 +13,54 @@ const HomePage = ({ location }) => {
       <SeoWrapper location={location} />
       <div className='player-wrapper'>
         <VideoPlayer location={location} />
+        {/* <div
+        className="menusnapp"
+        style={{
+          position: "absolute",
+          zIndex: "0",
+          top: "70px",
+          gap: "0",
+          padding: "2vh 2vw",
+          alignItems: "center",
+          // display: isMenuOpen ? "block" : "none",
+          background: "var(--theme-ui-colors-headerBackground)",
+          backgroundColor: "#222",
+          width: "100dvw",
+        }}
+      >
+        <div id="" className="flexbutt font" style={{ display: "", gap: "3vh", justifyContent: "center", alignItems: "center", margin: "0 0", padding: "0", position: "relative", minWidth: "80vw" }}>
+          <div style={{ minWidth: "25vw", maxHeight: "15vh", textAlign: "center", color: "#fff" }}>
+            <PirateLogo style={{ minWidth: "", maxHeight: "15vh", position: "", top: "", left: "" }} />
+            the web revolution
+          </div>
+          <div className="flexcheek mob2 print" style={{ position: "", top: "", minWidth: "25vw", overflow: "", marginBottom: "", paddingTop: "", borderRadius: "var(--theme-ui-colors-borderRadius)" }}>
+            <div className="nameblock font" style={{ margin: "0 auto 0 auto", padding: "0 0 0 0", alignContent: "center", display: "grid", textAlign: "center", justifyContent: "center", verticalAlign: "center", color: "#fff", paddingTop: "", fontSize: "clamp(1rem, 1.4vw, 3.2rem)", background: "rgba(0,0,0,0.50)", backdropFilter: "blur(8px)", border: "10px double var(--theme-ui-colors-buttonHoverBg)", borderRadius: "var(--theme-ui-colors-borderRadius)", textShadow: "0 2px 0px #000", maxWidth: "" }}>
+              <br />
+              <span style={{ margin: "2vh auto", fontSize: "160%" }}>About PIRATE</span>
+              <br />
+              A web revolution is coming
+              <br /><br />
+              And it's completely FREE!
+              <br />
+              <a href="https://pirateweb.org/about" className="button print" style={{ display: "flex", justifyContent: "center", padding: "1vh .5vw", maxWidth: "250px", margin: "30px auto", border:'1px solid #ff0000' }}>About PIRATE</a>
+            </div>
+          </div>
+          <div className="flexcheek mob2 print" style={{ position: "", top: "", minWidth: "25vw", overflow: "", marginBottom: "", paddingTop: "", borderRadius: "var(--theme-ui-colors-borderRadius)" }}>
+            <div className="nameblock font" style={{ margin: "0 auto 0 auto", padding: "0 0 0 0", alignContent: "center", display: "grid", textAlign: "center", justifyContent: "center", verticalAlign: "center", color: "#fff", paddingTop: "", fontSize: "clamp(1rem, 1.4vw, 3.2rem)", background: "rgba(0,0,0,0.50)", backdropFilter: "blur(8px)", border: "10px double var(--theme-ui-colors-buttonHoverBg)", borderRadius: "var(--theme-ui-colors-borderRadius)", textShadow: "0 2px 0px #000", maxWidth: "" }}>
+              <br />
+              <span style={{ margin: "2vh auto", fontSize: "160%" }}>Get PIRATE</span>
+              <br />
+              Web, Social &amp; Video Media
+              <br /><br />
+              Combined into your own app
+              <br />
+              <a href="https://pirateweb.org/install" className="button print" style={{ display: "flex", justifyContent: "center", padding: "1vh .5vw", maxWidth: "250px", margin: "30px auto", border:'1px solid #ff0000' }}>Become a PIRATE!</a>
+            </div>
+          </div>
+        </div>
+      </div> */}
       </div>
+
     </Layout>
   );
 };
